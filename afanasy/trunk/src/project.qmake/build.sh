d4bb1b74c4fd7718bee9cf2eff3ec1cfc662275c
timurhai <timurhai@0f86f178-d61e-0410-9d12-f6255c3815b8>
2010-04-28 11:56:52 +0000

    afanasy qmake build tries default 'qmake-qt4' command first
    git-svn-id: https://cgru.svn.sourceforge.net/svnroot/cgru@807 0f86f178-d61e-0410-9d12-f6255c3815b8

--- afanasy/trunk/src/project.qmake/build.sh
+++ afanasy/trunk/src/project.qmake/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Setup Qt:
-export QMAKE=qmake
+# Setup Qt (use system by default):
+export QMAKE=
 
 # Setup Python:
 export AF_PYTHON_INC="`python-config --cflags` -fPIC"
@@ -9,6 +9,24 @@ export AF_PYTHON_LIB="`python-config --ldflags`"
 
 # overrides (set custom values there):
 [ -f override.sh ] && source override.sh
+
+# Setup Qt if qmake not specified (try qt4 first):
+if [ -z "$QMAKE" ]; then
+   QMAKE=qmake-qt4
+   if ! qts=`which $QMAKE`; then
+      QMAKE=qmake
+      if ! qts=`which $QMAKE`; then
+         echo "ERROR: qmake not founded."
+         exit 1
+      fi
+   fi
+   for qt in $qts; do QMAKE=$qt; break; done
+fi
+if [ ! -f "$QMAKE" ]; then
+   echo "ERROR: qmake not founded."
+   exit 1
+fi
+echo "qmake = '$QMAKE'"
 
 # Run qmake to make makefiles
 $QMAKE
