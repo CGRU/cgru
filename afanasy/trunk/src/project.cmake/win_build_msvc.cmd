3482f53c379fa4b5d2c929da1b32dad0e4c94dc3
timurhai <timurhai@0f86f178-d61e-0410-9d12-f6255c3815b8>
2010-04-28 13:07:42 +0000

    windows build corrected
    git-svn-id: https://cgru.svn.sourceforge.net/svnroot/cgru@808 0f86f178-d61e-0410-9d12-f6255c3815b8

--- afanasy/trunk/src/project.cmake/win_build_msvc.cmd
+++ afanasy/trunk/src/project.cmake/win_build_msvc.cmd
@@ -1,10 +1,10 @@
-if exist override.cmd call override.cmd
-
 rem Cmake finds 'Qt' by searching for 'qmake' in 'PATH' at first:
 SET PATH=C:\Qt\4.6.2-vc2008\bin;%PATH%
 rem Specify exact python:
-rem SET AF_PYTHON_INCLUDE_PATH=C:\Python31\include
-rem SET AF_PYTHON_LIBRARIES=C:\Python31\libs\libpython31.a
+rem SET AF_PYTHON_INCLUDE_PATH=C:\Python25\include
+rem SET AF_PYTHON_LIBRARIES=C:\Python25\libs\python25.lib
+
+if exist override.cmd call override.cmd
 
 cmake .
 
