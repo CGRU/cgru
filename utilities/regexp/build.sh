2e8a8edf7e5b1d715fe341053880270074198cd7
timurhai <timurhai@0f86f178-d61e-0410-9d12-f6255c3815b8>
2010-04-29 07:56:45 +0000

    regexp utility build thies qmake-qt4 command first
    git-svn-id: https://cgru.svn.sourceforge.net/svnroot/cgru@810 0f86f178-d61e-0410-9d12-f6255c3815b8

--- utilities/regexp/build.sh
+++ utilities/regexp/build.sh
@@ -1,14 +1,31 @@
 #!/bin/bash
 
-# Setup Qt:
-export QMAKE=qmake
+# Setup Qt (use system by default):
+export QMAKE=
 
 # overrides (set custom values there):
 [ -f override.sh ] && source override.sh
 
-cd src
+# Setup Qt if qmake not specified (try qt4 first):
+if [ -z "$QMAKE" ]; then
+   QMAKE=qmake-qt4
+   if ! qts=`which $QMAKE`; then
+      QMAKE=qmake
+      if ! qts=`which $QMAKE`; then
+         echo "ERROR: qmake not founded."
+         exit 1
+      fi
+   fi
+   for qt in $qts; do QMAKE=$qt; break; done
+fi
+if [ ! -f "$QMAKE" ]; then
+   echo "ERROR: qmake not founded."
+   exit 1
+fi
+echo "qmake = '$QMAKE'"
 
 # Run qmake to make makefiles
+cd src
 $QMAKE
 
 # Make files
