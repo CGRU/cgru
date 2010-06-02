f71e0fae0ea0b624df59f2eec027cdc0f049c970
timurhai <timurhai@0f86f178-d61e-0410-9d12-f6255c3815b8>
2010-06-02 15:11:40 +0000

    pathes map
    git-svn-id: https://cgru.svn.sourceforge.net/svnroot/cgru@862 0f86f178-d61e-0410-9d12-f6255c3815b8

--- afanasy/trunk/src/include/afpynames.h
+++ afanasy/trunk/src/include/afpynames.h
@@ -11,9 +11,7 @@ namespace AFPYNAMES
    const char SERVICE_CLASSESDIR[]        = "services";     ///< Services directory relative to PYTHONPATH
    const char SERVICE_FUNC_GETWDIR[]      = "getWDir";
    const char SERVICE_FUNC_GETCOMMAND[]   = "getCommand";
-//   const char SERVICE_FUNC_APPLYCMDCAPACITY[] = "applycmdcapacity";
-//   const char SERVICE_FUNC_APPLYCMDHOSTS[]    = "applycmdhosts";
-   const char SERVICE_FUNC_CHECKFILES[]       = "checkfiles";
+   const char SERVICE_FUNC_CHECKFILES[]   = "checkfiles";
 
    const char PARSER_CLASSESDIR[] = "parsers";      ///< Parsers directory relative to PYTHONPATH
    const char PARSER_FUNC_PARSE[] = "parse";
