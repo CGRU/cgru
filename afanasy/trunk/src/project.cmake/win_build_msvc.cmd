7198d85a271abd9e876a6c8a11e2a85fa18d96f9
timurhai <timurhai@0f86f178-d61e-0410-9d12-f6255c3815b8>
2010-06-03 13:55:13 +0000

    afanasy build for win64
    git-svn-id: https://cgru.svn.sourceforge.net/svnroot/cgru@865 0f86f178-d61e-0410-9d12-f6255c3815b8

--- afanasy/trunk/src/project.cmake/win_build_msvc.cmd
+++ afanasy/trunk/src/project.cmake/win_build_msvc.cmd
@@ -1,8 +1,10 @@
 rem Cmake finds 'Qt' by searching for 'qmake' in 'PATH' at first:
-SET PATH=C:\Qt\4.6.2-vc2008\bin;%PATH%
+rem SET PATH=C:\Qt\4.6.2-vc2008\bin;%PATH%
 rem Specify exact python:
 rem SET AF_PYTHON_INCLUDE_PATH=C:\Python25\include
 rem SET AF_PYTHON_LIBRARIES=C:\Python25\libs\python25.lib
+
+cmake -G "Visual Studio 9 2008 Win64" .
 
 if exist override.cmd call override.cmd
 
