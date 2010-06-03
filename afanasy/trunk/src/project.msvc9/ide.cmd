55f1cc7860a94f656d0826ada31bdbc543e54584
timurhai <timurhai@0f86f178-d61e-0410-9d12-f6255c3815b8>
2010-06-03 13:56:54 +0000

    no msvc9 afanasy project, cmake can generate it, and no changes needed
    git-svn-id: https://cgru.svn.sourceforge.net/svnroot/cgru@866 0f86f178-d61e-0410-9d12-f6255c3815b8

--- afanasy/trunk/src/project.msvc9/ide.cmd
+++
@@ -1,5 +0,0 @@
-rem Setup environment:
-call setup.cmd
-
-rem Run Visual Studio IDE:
-"C:\Program Files\Microsoft Visual Studio 9.0\Common7\IDE\devenv.exe" afanasy.sln
--- afanasy/trunk/src/project.msvc9/setup.cmd
+++
@@ -1,7 +0,0 @@
-rem Setup environment
-
-rem Set Qt directory:
-SET QTDIR=C:\Qt\4.6.2-vs2008
-
-rem Set Python directory:
-SET PythonDir=C:\Python26
