f9b2790467c6190e2fe4893f67781c36bc3b4fc8
timurhai <timurhai@0f86f178-d61e-0410-9d12-f6255c3815b8>
2010-07-02 10:05:45 +0000

    logic script location dug
    git-svn-id: https://cgru.svn.sourceforge.net/svnroot/cgru@945 0f86f178-d61e-0410-9d12-f6255c3815b8

--- afanasy/trunk/plugins/xsi/afxsisubmit.py
+++ afanasy/trunk/plugins/xsi/afxsisubmit.py
@@ -108,11 +108,13 @@ def Main():
    oPPGLayout.EndRow()
 
    oPPGLayout.Language = 'Python'
-   logic_script = 'C:\\cg\\tools\\cgru\\afanasy\\trunk\\plugins\\xsi\\afxsisubmit_logic.py'
    afroot = os.getenv('AF_ROOT')
    if afroot is None or afroot == '':
       Application.LogMessage('AF_ROOT is not set, can`t find Afanasy.')
    else:
+      logic_script = os.path.join( afroot, 'plugins')
+      logic_script = os.path.join( afroot, 'xsi')
+      logic_script = os.path.join( afroot, 'afxsisubmit_logic.py')
       if( os.path.exists( logic_script)):
          textStream = open( logic_script,'r')
          logic = textStream.read()
