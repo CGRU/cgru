57f9ea74e2ec62d754eb8ab82455e3c31ede2fa6
timurhai <timurhai@0f86f178-d61e-0410-9d12-f6255c3815b8>
2010-08-12 09:50:40 +0000

    fix pathes
    git-svn-id: https://cgru.svn.sourceforge.net/svnroot/cgru@1036 0f86f178-d61e-0410-9d12-f6255c3815b8

--- utilities/fixpathes.py
+++ utilities/fixpathes.py
@@ -2,15 +2,20 @@
 
 import os, sys
 
-invalid_characters = ' :;![]()$%^&*`\\|/?"\''
+invalid_characters = ':;![]()$%^&*`\\|/?"\''
 replace_character  = '_'
 
 from optparse import OptionParser
 parser = OptionParser(usage="%prog [options] root\ntype \"%prog -h\" for help", version="%prog 1.  0")
 
-parser.add_option('-f', '--fix', dest='fix', action='store_true', default=False, help='Perform rename')
+parser.add_option('-c', '--capital', dest='capital',  action='store_true', default=False, help='Check capital letters.')
+parser.add_option('-s', '--spaces',  dest='spaces',   action='store_true', default=False, help='Check spaces.')
+parser.add_option('-i', '--invalid', dest='invalid',  action='store_true', default=False, help='Check invalid characters.')
+parser.add_option('-f', '--fix',     dest='fix',      action='store_true', default=False, help='Perform rename.')
 
 (options, args) = parser.parse_args()
+
+if not options.capital and not options.spaces and not options.invalid: parser.error('What to check, if no options provided?')
 
 if len( args) < 1: parser.error('Not enough arguments provided.')
 Root = args[0]
@@ -49,7 +54,12 @@ def rename( oldname, newname):
    except:
       print str(sys.exc_info()[1])
       result = False
-   print 'Ok.'
+
+   if os.path.lexists( oldname):
+      print 'Error: Old path still exists.'
+      result = False
+
+   if result: print 'Ok.'
 
    return result
 
@@ -58,18 +68,26 @@ for dirpath, dirnames, filenames in os.walk( Root, False, None):
       fixedname = dirname
       invalid = False
 
-      # Check for invalid characterss:
-      for char in dirname:
-         if char in invalid_characters:
-            fixedname = fixName( fixedname)
+      # Check for spaces:
+      if options.spaces:
+         if fixedname.find(' ') != -1:
+            fixedname = fixedname.replace(' ', replace_character)
             invalid = True
-            break
+
+      # Check for invalid characterss:
+      if options.invalid:
+         for char in dirname:
+            if char in invalid_characters:
+               fixedname = fixName( fixedname)
+               invalid = True
+               break
 
       # Check for capital letters:
-      loweredname = fixedname.lower()
-      if fixedname != loweredname:
-         fixedname = loweredname
-         invalid = True
+      if options.capital:
+         loweredname = fixedname.lower()
+         if fixedname != loweredname:
+            fixedname = loweredname
+            invalid = True
 
       # Skip if no rename needed:
       if not invalid: continue
