186ac7f29184eed5b3775fb6d0a68a2629184b49
timurhai <timurhai@0f86f178-d61e-0410-9d12-f6255c3815b8>
2010-08-16 14:17:32 +0000

    windows child process resumes process, windows qtcore should be pathed to start process suspended
    git-svn-id: https://cgru.svn.sourceforge.net/svnroot/cgru@1048 0f86f178-d61e-0410-9d12-f6255c3815b8

--- afanasy/trunk/src/render/childprocess.cpp
+++ afanasy/trunk/src/render/childprocess.cpp
@@ -20,6 +20,13 @@ WINBASEAPI HANDLE WINAPI CreateJobObjectA(LPSECURITY_ATTRIBUTES,LPCSTR);
 ChildProcess::ChildProcess( QObject * parent):
    QProcess(parent)
 {
+#ifdef WINNT
+   hJob = CreateJobObject( NULL, NULL);
+   JOBOBJECT_EXTENDED_LIMIT_INFORMATION jeli = { 0 };
+   jeli.BasicLimitInformation.LimitFlags = JOB_OBJECT_LIMIT_KILL_ON_JOB_CLOSE; //0x00002000;
+   if( SetInformationJobObject( hJob, JobObjectExtendedLimitInformation, &jeli, sizeof( jeli ) ) == 0)
+      AFERROR("SetInformationJobObject failed.\n");
+#endif
 }
 
 ChildProcess::~ChildProcess()
@@ -36,13 +43,9 @@ void ChildProcess::setupChildProcess()
 #else
    process_info = pid();
    SetPriorityClass( process_info->hProcess, BELOW_NORMAL_PRIORITY_CLASS);
-   hJob = CreateJobObject( NULL, NULL);
-   JOBOBJECT_EXTENDED_LIMIT_INFORMATION jeli = { 0 };
-   jeli.BasicLimitInformation.LimitFlags = JOB_OBJECT_LIMIT_KILL_ON_JOB_CLOSE; //0x00002000;
-   if( SetInformationJobObject( hJob, JobObjectExtendedLimitInformation, &jeli, sizeof( jeli ) ) == 0)
-      AFERROR("SetInformationJobObject failed.\n");
    if( AssignProcessToJobObject( hJob, process_info->hProcess) == false)
       AFERROR("AssignProcessToJobObject failed.\n");
+   ResumeThread( process_info->hThread);
 #endif
 }
 
