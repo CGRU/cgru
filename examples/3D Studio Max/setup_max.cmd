d04b879d3b735bb948b8400316cf593ef9847249
timurhai <timurhai@0f86f178-d61e-0410-9d12-f6255c3815b8>
2010-09-20 16:43:03 +0000

    3dmax example
    git-svn-id: https://cgru.svn.sourceforge.net/svnroot/cgru@1098 0f86f178-d61e-0410-9d12-f6255c3815b8

--- examples/3D Studio Max/setup_max.cmd
+++ examples/3D Studio Max/setup_max.cmd
@@ -4,7 +4,7 @@ popd
 
 set MAX_LOCATION=C:\Program Files\Autodesk\3ds Max 2010
 
-PATH=%CGRU_LOCATION%\plugins\max;%PATH%
+set PATH=%CGRU_LOCATION%\plugins\max;%PATH%
 
 if exist override.cmd call override.cmd
 
