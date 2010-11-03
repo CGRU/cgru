135dfce6c4bb2f884957bd72685ec82c8c05fbd0
timurhai <timurhai@0f86f178-d61e-0410-9d12-f6255c3815b8>
2010-11-03 14:24:57 +0000

    xsi varirender crates dir for each framebuffer
    git-svn-id: https://cgru.svn.sourceforge.net/svnroot/cgru@1185 0f86f178-d61e-0410-9d12-f6255c3815b8

--- plugins/xsi/afrender.py
+++ plugins/xsi/afrender.py
@@ -25,17 +25,35 @@ def afRender( scene, start, end, step, simulate, renderPass = '', attr = '', val
       Application.SetValue('PlayControl.Current', end )
       Application.SetValue('PlayControl.In', start )
       Application.SetValue('PlayControl.Out', end )
-      log('Trying to render current pass "%s"...' % renderPass)
+   if renderPass != '': log('Trying to render current pass "%s"...' % renderPass)
    if attr != '':
       log('Trying to set value "%s" to "%s"...' % ( attr, value))
       value = int(value)
       Application.SetValue( attr, value)
-      outdir = Application.GetValue('Passes.RenderOptions.ResolvedOutputDir')
-      outdir = os.path.join( outdir, str(value))
-      log('Changing output directory to:')
-      log( outdir)
-      if not os.path.isdir( outdir): os.mkdir( outdir)
-      Application.SetValue('Passes.RenderOptions.OutputDir', outdir)
+      changeRenderOptionsOutputDir = True
+      if renderPass != '':
+         for ps in Application.ActiveProject.ActiveScene.Passes:
+            if ps.Name != renderPass: continue
+            changeRenderOptionsOutputDir = False
+            for fb in ps.Framebuffers:
+               if not fb.Enabled.Value: continue
+               fbpath = "%s" % fb.Filename.Value
+               outfile = os.path.dirname( fbpath)
+               if outfile == '':
+                  changeRenderOptionsOutputDir = True
+                  continue
+               outfile = os.path.join( outfile, str(value))
+               if not os.path.isdir( outfile): os.makedirs( outfile)
+               outfile = os.path.join( outfile, os.path.basename( fbpath))
+               log('Path chnaged: ' + outfile)
+               Application.SetValue("Passes.%s.%s.Filename" % (renderPass, fb.Name), outfile)
+      if changeRenderOptionsOutputDir:
+         outdir = Application.GetValue('Passes.RenderOptions.ResolvedOutputDir')
+         outdir = os.path.join( outdir, str(value))
+         log('Changing output directory to:')
+         log( outdir)
+         if not os.path.isdir( outdir): os.makedirs( outdir)
+         Application.SetValue('Passes.RenderOptions.OutputDir', outdir)
 
    Application.Refresh()
    if renderPass != '': renderPass = 'Passes.' + renderPass
