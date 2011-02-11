9f1e80d015cc4ef92e2e6633bf511be8df48579a
timurhai <timurhai@0f86f178-d61e-0410-9d12-f6255c3815b8>
2011-02-11 20:26:55 +0000

    mantra parser regular expressions check
    git-svn-id: https://cgru.svn.sourceforge.net/svnroot/cgru@1311 0f86f178-d61e-0410-9d12-f6255c3815b8

--- afanasy/trunk/python/parsers/mantra.py
+++ afanasy/trunk/python/parsers/mantra.py
@@ -1,10 +1,13 @@
 import parser
 
+import re
+
 PERCENT = 'ALF_PROGRESS '
 PERCENT_len = len(PERCENT)
 
 Warnings = ['Unable to access file','Unable to load texture']
-Errors = ['No licenses could be found to run this application','Please check for a valid license server host']
+Errors = ['No licenses could be found to run this application','Please check for a valid license server host','Failed to create file']
+ErrorsRE = [re.compile(r'Error loading geometry .* from stdin')]
 
 class mantra(parser.parser):
    'Houdini mantra with "Alfred Style Progress"'
@@ -14,12 +17,17 @@ class mantra(parser.parser):
    def do( self, data):
 
       for warning in Warnings:
-         if data.find(warning) == 0:
+         if data.find(warning) != -1:
             self.warning = True
             break
 
       for error in Errors:
-         if data.find(error) == 0:
+         if data.find(error) != -1:
+            self.error = True
+            break
+
+      for errorRE in ErrorsRE:
+         if errorRE.search( data) is not None:
             self.error = True
             break
 
