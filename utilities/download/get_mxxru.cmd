1fe7305a52581743653f90fb3cde94e5889ad0e2
timurhai <timurhai@0f86f178-d61e-0410-9d12-f6255c3815b8>
2011-02-24 15:25:35 +0000

    windows ruby version is 1.9.1 and Mxx_ru is 1.5.2
    git-svn-id: https://cgru.svn.sourceforge.net/svnroot/cgru@1357 0f86f178-d61e-0410-9d12-f6255c3815b8

--- utilities/download/get_mxxru.cmd
+++ utilities/download/get_mxxru.cmd
@@ -1,5 +1,5 @@
-set ver=1.4.10
-set location=http://rubyforge.org/frs/download.php/53915
+set ver=1.5.2
+set location=http://rubyforge.org/frs/download.php/70936
 set filename=Mxx_ru-%ver%.gem
 set link=%location%/%filename%
 
--- utilities/download/get_ruby.cmd
+++ utilities/download/get_ruby.cmd
@@ -1,6 +1,6 @@
-set filename=rubyinstaller-1.8.7-p334.exe
+set filename=rubyinstaller-1.9.1-p430.exe
 
-set location=http://rubyforge.org/frs/download.php/74293
+set location=http://rubyforge.org/frs/download.php/72075
 
 if exist %filename% (
    echo File %filename% already exists. Skipping download.
