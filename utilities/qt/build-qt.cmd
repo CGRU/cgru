bd9ac2652548a44c30c2d02d11db347d1c492354
timurhai <timurhai@0f86f178-d61e-0410-9d12-f6255c3815b8>
2011-03-11 22:00:21 +0000

    build default win qt added
    git-svn-id: https://cgru.svn.sourceforge.net/svnroot/cgru@1386 0f86f178-d61e-0410-9d12-f6255c3815b8

---
+++ utilities/qt/build-qt.cmd
@@ -0,0 +1,16 @@
+set VSINSTALLDIR="C:\Program Files (x86)\Microsoft Visual Studio 9.0"
+set VCVARS=amd64
+set qt=qt-everywhere-opensource-src-4.7.1
+
+if exist override.cmd call override.cmd
+
+call %VSINSTALLDIR%\VC\vcvarsall.bat %VCVARS%
+
+python patch-qprocess_win.cpp.py %qt%
+
+cd %qt%
+
+configure.exe -platform win32-msvc2008 -release -opensource -static -no-scripttools -no-multimedia -no-script -no-webkit -nomake examples -nomake demos -no-sql-sqlite -no-gif
+nmake
+
+pause
