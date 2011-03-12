1c9ddecd92c5bafcb4bf6793522028295ca6ae8e
timurhai <timurhai@0f86f178-d61e-0410-9d12-f6255c3815b8>
2011-03-12 11:57:22 +0000

    build pyafs mxxru cleanup fixed
    git-svn-id: https://cgru.svn.sourceforge.net/svnroot/cgru@1390 0f86f178-d61e-0410-9d12-f6255c3815b8

--- afanasy/trunk/src/project.mxxru.pyafs/clear.py
+++ afanasy/trunk/src/project.mxxru.pyafs/clear.py
@@ -7,5 +7,9 @@ todelete = 'tmp'
 
 for afile in os.listdir( os.getcwd()):
    if afile.find( todelete) == 0:
-      print 'Deleting "%s"' % afile
-      shutil.rmtree( afile)
+      if os.path.isdir( afile):
+         print 'Deleting folder "%s"' % afile
+         shutil.rmtree( afile)
+      elif os.path.isfile( afile):
+         print 'Deleting file "%s"' % afile
+         os.remove( afile)
