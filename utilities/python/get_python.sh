f296ede69e8aae2e162fa21d4d7475cb364f7ef3
timurhai <timurhai@0f86f178-d61e-0410-9d12-f6255c3815b8>
2011-03-17 14:03:03 +0000

    get python default version
    git-svn-id: https://cgru.svn.sourceforge.net/svnroot/cgru@1428 0f86f178-d61e-0410-9d12-f6255c3815b8

--- utilities/python/get_python.sh
+++ utilities/python/get_python.sh
@@ -1,22 +1,12 @@
 #!/bin/bash
 
 ver=$1
+[ -z "$ver" ] && ver=2.7.1
 location="http://www.python.org/ftp/python"
 foldername="Python-$ver"
 archive_ext="tar.bz2"
 archive="$foldername.$archive_ext"
 link="$location/$ver/$archive"
-
-function usageErrorExit(){
-   echo "Usage:"
-   echo "$0 [python version]"
-   exit 1
-}
-
-if [ -z "$ver" ] ; then
-   echo "Python version is not set."
-   usageErrorExit
-fi
 
 wget $link
 
