970d5bcbcbcff0341816f7c5631d5705985c10a7
timurhai <timurhai@0f86f178-d61e-0410-9d12-f6255c3815b8>
2011-05-26 14:16:56 +0000

    blender example fixes
    git-svn-id: https://cgru.svn.sourceforge.net/svnroot/cgru@1609 0f86f178-d61e-0410-9d12-f6255c3815b8

--- examples/blender2.5/setup_blender.sh
+++ examples/blender2.5/setup_blender.sh
@@ -6,21 +6,12 @@ popd >> /dev/null
 
 source $AF_ROOT/py3k_setup.sh 3.2-utf32
 
-export BLENDER_USER_SCRIPTS="$AF_ROOT/plugins/blender25"
-export BLENDER_SYSTEM_SCRIPTS="$AF_ROOT/plugins/blender25"
+#export BLENDER_USER_SCRIPTS="$AF_ROOT/plugins/blender2.5"
+#export BLENDER_SYSTEM_SCRIPTS="$AF_ROOT/plugins/blender2.5"
 
 export AF_CMD_PREFIX="./"
 
-[ -f override.sh ] && source override.sh
+export BLENDER_LOCATION="/opt/blender-2.57b-linux-glibc27-x86_64"
 
-#afscripts_cgru=$AF_ROOT/plugins/blender
-#afscripts_blender=$HOME/.blender/scripts/afanasy
-#if [ -L $afscripts_blender ]; then
-#   if [ "`readlink $afscripts_blender`" != $afscripts_cgru ]; then
-#      rm -vf $afscripts_blender
-#   fi
-#fi
-#if [ ! -d $afscripts_blender ]; then
-#   echo "Linking Afanasy scripts folder:"
-#   ln -svf $afscripts_cgru $afscripts_blender
-#fi
+# Redefine blender location in "override.sh" file:
+[ -f override.sh ] && source override.sh
