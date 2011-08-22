2f53c743cc6a992f315098bf0554e264cd84f1ed
timurhai <timurhai@0f86f178-d61e-0410-9d12-f6255c3815b8>
2011-08-22 12:10:41 +0000

    win soft versions
    git-svn-id: https://cgru.svn.sourceforge.net/svnroot/cgru@1668 0f86f178-d61e-0410-9d12-f6255c3815b8

--- utilities/download/get_pyqt.cmd
+++ utilities/download/get_pyqt.cmd
@@ -1,4 +1,4 @@
-set filename=PyQt-Py2.7-x64-gpl-4.8.3-1.exe
+set filename=PyQt-Py2.7-x64-gpl-4.8.5-1.exe
 set location=http://www.riverbankcomputing.co.uk/static/Downloads/PyQt4
 if exist %filename% (
    echo File %filename% already exists. Skipping download.
