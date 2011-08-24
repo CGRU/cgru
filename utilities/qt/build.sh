2e9680c10f9ff9b7a0b89778729c143c9ad98e67
timurhai <timurhai@0f86f178-d61e-0410-9d12-f6255c3815b8>
2011-08-24 13:58:01 +0000

    qt mac build
    git-svn-id: https://cgru.svn.sourceforge.net/svnroot/cgru@1676 0f86f178-d61e-0410-9d12-f6255c3815b8

--- utilities/qt/build.sh
+++ utilities/qt/build.sh
@@ -9,12 +9,20 @@ if [ ! -d $qtsrc ]; then
    exit 1
 fi
 
+flags="-prefix $qtdir -v -opensource -release -static -qt-sql-psql -nomake examples -nomake demos -no-qt3support -no-xmlpatterns -no-multimedia -no-audio-backend -no-phonon -no-webkit -no-javascript-jit -no-script -no-scripttools -no-declarative -no-gif -no-openssl"
+if [ `uname` == "Darwin" ]; then
+   echo "Darwin framework disabled."
+   flags="$flags -no-framework -arch x86_64"
+fi
+
+echo "Flags: $flags"
+
 cd $qtsrc
 
 if [ "$1" == "-h" ]; then
    ./configure -h
 else
-   ./configure -prefix $qtdir -v -opensource -release -static -qt-sql-psql -nomake examples -nomake demos -no-qt3support -no-xmlpatterns -no-multimedia -no-audio-backend -no-phonon -no-webkit -no-javascript-jit -no-script -no-scripttools -no-declarative -no-gif -no-openssl "$@"
+   ./configure $flags "$@"
    make
    make install
 fi
