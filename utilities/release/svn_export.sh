b7ceca732e8bd22606b6d9a153799b828760196e
timurhai <timurhai@0f86f178-d61e-0410-9d12-f6255c3815b8>
2011-08-29 17:11:23 +0000

    release last changes
    git-svn-id: https://cgru.svn.sourceforge.net/svnroot/cgru@1685 0f86f178-d61e-0410-9d12-f6255c3815b8

--- utilities/release/svn_export.sh
+++ utilities/release/svn_export.sh
@@ -12,12 +12,14 @@ popd > /dev/null
 
 # Exporting current subversion:
 cgruSVN=cgru_svn${packsrev}
-tmpdir=/tmp
+tmpdir=tmp
+[ -d $tmpdir ] || mkdir -pv $tmpdir
 cgruSVN_dir=$tmpdir/$cgruSVN
-echo "Exporting CGRU revision $packsrev"
 [ -d $cgruSVN_dir ] && rm -rf $cgruSVN_dir
+echo "Exporting CGRU revision $packsrev"
 svn export $cgruRoot $cgruSVN_dir
 cd $tmpdir
 acrhivename=$curdir/$cgruSVN.7z
 [ -f $acrhivename ] && rm -fv $acrhivename
+echo "Compressing $acrhivename"
 7za a -r -y -t7z $acrhivename $cgruSVN > /dev/null || echo "Failed!"
