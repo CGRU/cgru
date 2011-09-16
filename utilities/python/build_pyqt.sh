2d7e9b9ba52ae79c7abd2ad568526ea8889304ef
timurhai <timurhai@0f86f178-d61e-0410-9d12-f6255c3815b8>
2011-09-16 11:24:49 +0000

    buld pyqt
    git-svn-id: https://cgru.svn.sourceforge.net/svnroot/cgru@1756 0f86f178-d61e-0410-9d12-f6255c3815b8

--- utilities/python/build_pyqt.sh
+++ utilities/python/build_pyqt.sh
@@ -5,15 +5,21 @@ if [ -z "$pythonver" ]; then
    pythonver="2.7.2"
 fi
 
+qtver=$2
+if [ -z "$qtver" ]; then
+qtver="4.7.4"
+fi
+
 pythondir=$PWD/$pythonver
 if [ ! -d "$pythondir" ]; then
    echo "Error: No python '$pythondir' founded."
    exit 1
 fi
 
-qt=`dirname $PWD`/qt/4.7.4
+qt=`dirname $PWD`/qt/$qtver
 
 export PATH=$pythondir/bin:$qt/bin:$PATH
+echo "PATH=$PATH"
 
 if [[ "$pythonver" > "3" ]]; then
    python="python3"
@@ -26,7 +32,7 @@ cd PyQt-x11-gpl-4.8.5
 flags="-g --confirm-license"
 flags="$flags -e QtCore -e QtGui -e QtSvg"
 
-if [ ! -z "$2" ]; then
+if [ ! -z "$3" ]; then
    $python configure.py -h
 else
    $python configure.py $flags
