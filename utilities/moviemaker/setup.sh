e42082c82fad2aa8b2ed4cb95dda130a59dda693
timurhai <timurhai@0f86f178-d61e-0410-9d12-f6255c3815b8>
2011-09-16 12:07:26 +0000

    default python version
    git-svn-id: https://cgru.svn.sourceforge.net/svnroot/cgru@1759 0f86f178-d61e-0410-9d12-f6255c3815b8

--- utilities/moviemaker/setup.sh
+++ utilities/moviemaker/setup.sh
@@ -6,7 +6,7 @@ cd ../..
 source ./setup.sh
 popd >> /dev/null
 
-cgru_python=$CGRU_LOCATION/utilities/python/2.7.1
+cgru_python=$CGRU_LOCATION/utilities/python/2.7.2
 
 if [ -d $cgru_python ]; then
    export PATH=:$cgru_python/bin:$PATH
