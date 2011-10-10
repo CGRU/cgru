9dc2bdb9cc80b86dd9587467a4dd0278f1658531
timurhai <timurhai@0f86f178-d61e-0410-9d12-f6255c3815b8>
2011-10-10 10:08:13 +0000

    python setup
    git-svn-id: https://cgru.svn.sourceforge.net/svnroot/cgru@1822 0f86f178-d61e-0410-9d12-f6255c3815b8

--- setup_python.sh
+++ setup_python.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 if [ -z "${CGRU_PYTHONEXE}" ]; then
+   export CGRU_PYTHONEXE="python"
    python="${CGRU_LOCATION}/python"
    if [ -d "${python}" ]; then
       echo "Using CGRU Python: ${python}"
       export PYTHONHOME="${python}"
       export PATH="$python/bin:${PATH}"
       export CGRU_PYTHONDIR="${python}"
-      export CGRU_PYTHONEXE="${python}/bin/python3"
-   else
-      export CGRU_PYTHONEXE="python"
+      pythonexe="${python}/bin/python3"
+      [ -f "${pythonexe}" ] && export CGRU_PYTHONEXE="${python}/bin/python3"
    fi
 fi
