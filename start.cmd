15974f806f6c4dd973963e9bca5b5ccb7a864f67
timurhai <timurhai@0f86f178-d61e-0410-9d12-f6255c3815b8>
2011-10-11 09:17:52 +0000

    keeper
    git-svn-id: https://cgru.svn.sourceforge.net/svnroot/cgru@1836 0f86f178-d61e-0410-9d12-f6255c3815b8

--- start.cmd
+++ start.cmd
@@ -5,6 +5,7 @@ call setup.cmd
 set keeper=%CGRU_LOCATION%\utilities\keeper\keeper.py
 
 if [%1] == [] (
+   set CGRU_KEEPER_CMD="%CGRU_PYTHONDIR%\pythonw.exe" "%keeper%"
    start "Keeper" "%CGRU_PYTHONDIR%\pythonw.exe" "%keeper%"
 ) else (
    "%CGRU_PYTHONEXE%" "%keeper%"
