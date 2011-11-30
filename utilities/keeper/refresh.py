2db4e65cbfdf2f3107e0e09c8fbe5c563a6b9140
timurhai <timurhai@0f86f178-d61e-0410-9d12-f6255c3815b8>
2011-11-30 08:48:27 +0000

    trayrefresh
    git-svn-id: https://cgru.svn.sourceforge.net/svnroot/cgru@1968 0f86f178-d61e-0410-9d12-f6255c3815b8

--- utilities/keeper/refresh.py
+++ utilities/keeper/refresh.py
@@ -11,15 +11,16 @@ class Refresh:
    def __init__( self, application):
       self.timer = QtCore.QTimer( application)
       QtCore.QObject.connect( self.timer, QtCore.SIGNAL('timeout()'), self.refresh)
-      self.timer.setInterval( 1000 * int(cgruconfig.VARS['keeper_refresh']))
+      self.timer.setInterval( 1000 * 3)
       self.timer.start()
       self.counter = 0
       self.refresh()
 
    def refresh( self):
-      if self.counter > 0: cgruconfig.Config()
-      if self.timer.interval() != 1000 * int(cgruconfig.VARS['keeper_refresh']):
-         self.timer.setInterval( 1000 * int(cgruconfig.VARS['keeper_refresh']))
+      if self.counter > 0:
+         cgruconfig.Config()
+         if self.timer.interval() != 1000 * int(cgruconfig.VARS['keeper_refresh']):
+            self.timer.setInterval( 1000 * int(cgruconfig.VARS['keeper_refresh']))
       nimby.refresh()
       render.refresh()
       self.counter += 1
