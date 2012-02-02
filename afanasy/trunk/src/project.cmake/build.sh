5905a98c981c630a102e7d9cfc1bde229a279386
timurhai <timurhai@0f86f178-d61e-0410-9d12-f6255c3815b8>
2012-02-02 16:22:50 +0000

    cmake add modules - linux distr depended
    git-svn-id: https://cgru.svn.sourceforge.net/svnroot/cgru@2143 0f86f178-d61e-0410-9d12-f6255c3815b8

--- afanasy/trunk/src/project.cmake/build.sh
+++ afanasy/trunk/src/project.cmake/build.sh
@@ -12,31 +12,33 @@ export AF_ADD_LFLAGS="-lutil"
 
 echo "Building on '${DISTRIBUTIVE}'"
 case ${DISTRIBUTIVE} in
-   openSUSE)
-      export AF_ADD_LFLAGS="$AF_ADD_LFLAGS -lpthread"
-      ;;
-   Debian)
-      ;;
-   Gentoo)
-      ;;
-   Ubuntu)
-      ;;
-   Fedora)
-      export AF_ADD_LFLAGS="$AF_ADD_LFLAGS -lpthread"
-      ;;
-   AltLinux)
-      ;;
-   CentOS)
-      ;;
-   *)
-      echo "Warning: Untested system: '${DISTRIBUTIVE}'"
-      ;;
+    openSUSE)
+        export AF_ADD_LFLAGS="$AF_ADD_LFLAGS -lpthread"
+        ;;
+    Debian)
+        export ADD_CMAKE_MODULE_PATH="$PWD"
+        ;;
+    Gentoo)
+        ;;
+    Ubuntu)
+        export ADD_CMAKE_MODULE_PATH="$PWD"
+        ;;
+    Fedora)
+        export AF_ADD_LFLAGS="$AF_ADD_LFLAGS -lpthread"
+        ;;
+    AltLinux)
+        export ADD_CMAKE_MODULE_PATH="$PWD"
+        ;;
+    CentOS)
+        export ADD_CMAKE_MODULE_PATH="$PWD"
+        ;;
+    *)
+        echo "Warning: Untested system: '${DISTRIBUTIVE}'"
+        ;;
 esac
 
 # export AF_PYTHON_INCLUDE_PATH=C:\Python31\include
 # export AF_PYTHON_LIBRARIES=C:\Python31\libs\libpython31.a
-
-export ADD_CMAKE_MODULE_PATH="$PWD"
 
 [ -f override.sh ] && source override.sh
 
