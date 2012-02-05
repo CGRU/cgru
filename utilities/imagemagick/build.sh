2ba7a9291ebb35788c31a1ea8003ae3eb4cb7c48
timurhai <timurhai@0f86f178-d61e-0410-9d12-f6255c3815b8>
2012-02-05 19:25:26 +0000

    imagemagick build script fixed
    git-svn-id: https://cgru.svn.sourceforge.net/svnroot/cgru@2151 0f86f178-d61e-0410-9d12-f6255c3815b8

--- utilities/imagemagick/build.sh
+++ utilities/imagemagick/build.sh
@@ -7,8 +7,7 @@ exr=`dirname $PWD`/openexr
 prefix=$PWD
 
 export OPENEXR_CFLAGS="-I$exr/include/OpenEXR"
-export OPENEXR_LIBS="-L$exr/lib -L$exr/lib64 -lIlmImf -lz -lImath -lHalf -lIex 
--lIlmThread -lpthread"
+export OPENEXR_LIBS="-L$exr/lib -L$exr/lib64 -lIlmImf -lz -lImath -lHalf -lIex -lIlmThread -lpthread"
 export LDFLAGS="-B/usr/lib/gold-ld/ -lstdc++"
 
 cd $im
