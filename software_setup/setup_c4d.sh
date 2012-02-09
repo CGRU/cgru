da3cb9cabd594f0b6511820cff5a66721f5cfb3e
janoberhauser <janoberhauser@0f86f178-d61e-0410-9d12-f6255c3815b8>
2012-02-09 18:25:52 +0000

    Now C4D finds itself on MacOS.
    git-svn-id: https://cgru.svn.sourceforge.net/svnroot/cgru@2173 0f86f178-d61e-0410-9d12-f6255c3815b8

--- software_setup/setup_c4d.sh
+++ software_setup/setup_c4d.sh
@@ -5,20 +5,23 @@ source "$CGRU_LOCATION/software_setup/setup__all.sh"
 
 
 # Locate C4D:
-C4F_LOCATION="/Applications/MAXON/CINEMA 4D R13/CINEMA 4D.app/Contents/MacOS"
+C4D_LOCATION=""
 C4D_EXEC="CINEMA 4D"
-C4D_USER_FOLDER="${HOME}/Library/Preferences/MAXON/CINEMA 4D R13_DA28FA2D"
+C4D_USER_FOLDER=""
+C4D_USER_FOLDER_LOCATION="${HOME}/Library/Preferences/MAXON"
+
+# Find the most recent C4D-Version-User-Folder
+STDOUT=$(ls -1 "$C4D_USER_FOLDER_LOCATION")
+while read line; do
+    if [ "`echo $line | awk '{print match( \$1, "CINEMA")}'`" == "1" ]; then
+        C4D_USER_FOLDER="${C4D_USER_FOLDER_LOCATION}/${line}"
+    fi
+done <<< "$STDOUT"
 
 # Here additional folders can get specified where plugins should get copied from. Should get specified outside that file.
 # This gets read by the python_init.py
 #export C4D_ADDITIONAL_PLUGIN_FOLDERS="PATH1;PATH2"
 
-
-#override c4d location based on locate_nuke.sh
-locate_c4d="$CGRU_LOCATION/software_setup/locate_c4d.sh"
-[ -f $locate_c4d ] && source $locate_c4d
-
-export C4D_EXEC="${C4F_LOCATION}/${C4D_EXEC}"
 export C4D_LOCATION
 export C4D_USER_FOLDER
 
@@ -31,6 +34,23 @@ export C4D_AF_SCRIPTS_LOCATION=$AF_ROOT/plugins/c4d
 # Local ones on client
 export C4D_PLUGIN_LOCATION=${C4D_USER_FOLDER}/plugins
 export C4D_PREFERENCES_LOCATION=${C4D_USER_FOLDER}/prefs
+
+C4D_INSTALL_DIR="/Applications/MAXON"
+
+
+# Find the most recent C4D-Version
+STDOUT=$(ls -1 "$C4D_INSTALL_DIR")
+while read line; do
+    if [ "`echo $line | awk '{print match( \$1, "CINEMA")}'`" == "1" ]; then
+        C4D_LOCATION="${C4D_INSTALL_DIR}/${line}/CINEMA 4D.app/Contents/MacOS"
+    fi
+done <<< "$STDOUT"
+
+
+#override c4d location based on locate_nuke.sh
+locate_c4d="$CGRU_LOCATION/software_setup/locate_c4d.sh"
+[ -f $locate_c4d ] && source $locate_c4d
+
 
 # Define that the c4d-render-script can get found. Gets used by "c4drender"
 export C4D_RENDER_SCRIPT="${C4D_CGRU_LOCATION}/render.py"
@@ -45,8 +65,8 @@ else
     echo "       Please set C4D_USER_FOLDER to point to the User-Directory!"
 fi
 
-export APP_DIR="${C4F_LOCATION}"
+export C4D_EXEC="${C4D_LOCATION}/${C4D_EXEC}"
+export APP_DIR="${C4D_LOCATION}"
 export APP_EXE="${C4D_EXEC}"
-
 
 echo "C4D = '${C4D_EXEC}'"
