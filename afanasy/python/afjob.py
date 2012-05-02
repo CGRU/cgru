91a0d8f21d6150940efdccde5a2176afe68bc71f
timurhai <timurhai@gmail.com>
2012-05-02 20:18:09 +0300

    maya2012+mentalrey use all cpu theads flag changed

--- afanasy/python/afjob.py
+++ afanasy/python/afjob.py
@@ -441,7 +441,7 @@ elif (ext == 'mb') or (ext == 'ma'):
       images = output
    cmd += ' -proj "%s"' % pwd
    if scenetype == 'maya_mental':
-      cmd += ' "-mr:art"'
+      cmd += ' -art'
    if extrargs != '': cmd += ' ' + extrargs
    cmd += ' "%s"' % scene
 
