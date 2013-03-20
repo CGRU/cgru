e18dbfb11fd90cda67ecd5687973d94e59ae1f85
timurhai <timurhai@gmail.com>
2013-03-20 21:17:09 +0400

    nukeview

--- plugins/nuke/view.py
+++ plugins/nuke/view.py
@@ -1,6 +1,14 @@
-import os, re, sys
+import optparse, os, re, sys
 
-Input = sys.argv[-1]
+# Parse arguments:
+Parser = optparse.OptionParser( usage="usage: %prog [options] (like nuke --help)", version="%prog 1.0")
+Parser.add_option('-o', '--open', dest='open', action='store_true', default=False, help='Open Nuke and create read nodes.')
+Parser.add_option('-c', '--copy', dest='copy', action='store_true', default=False, help='Copy readers for paste in Nuke.')
+(Options, Args) = Parser.parse_args()
+
+if len( Args ) < 1: Parser.error('Input is not provided.')
+
+Input = Args[-1]
 
 print( Input)
 
@@ -38,15 +46,43 @@ if os.path.isdir( InDir):
 
 InDir = InDir.replace('\\','/')
 
+if Options.open:
+	nuke = __import__('nuke', globals(), locals(), [])
+if Options.copy:
+	CopyText = 'set cut_paste_input [stack 0]\n'
+	cgrupyqt = __import__('cgrupyqt', globals(), locals(), [])
+	qapp = cgrupyqt.QtGui.QApplication([])
+
 for i in range( len(Prefixes)):
 	print('%s[%d-%d/%d]%s' % (Prefixes[i], Starts[i], Ends[i], Paddings[i], Suffixes[i]))
 	filename = Prefixes[i] + '#'*Paddings[i] + Suffixes[i]
 	filename = InDir + '/' + filename
 	filename = filename.replace('//','/')
 	print( filename)
-	nuke = __import__('nuke', globals(), locals(), [])
-	node = nuke.createNode('Read')
-	node.knob('file').setValue( filename)
-	node.knob('first').setValue( Starts[i])
-	node.knob('last').setValue( Ends[i])
+	if Options.open:
+		node = nuke.createNode('Read')
+		node.knob('file').setValue( filename)
+		node.knob('first').setValue( Starts[i])
+		node.knob('last').setValue( Ends[i])
+	if Options.copy:
+		CopyText += 'Read {\n'
+		CopyText += ' inputs 0\n'
+		CopyText += ' file %s\n' % filename
+		CopyText += ' first %d\n' % Starts[i]
+		CopyText += ' last %d\n' % Ends[i]
+		CopyText += ' selected true\n'
+		CopyText += '}\n'
+
+
+if Options.copy:
+	print('Copying:')
+	print( CopyText)
+	qapp.clipboard().setText( CopyText)
+	cgrupyqt.QtCore.QTimer.singleShot( 10000, qapp.quit)
+#	qapp.processEvents()
+#	qapp.sendPostedEvents()
+#	clipboard = cgrupyqt.QtGui.QApplication.clipboard()
+#	event = cgrupyqt.QtCore.QEvent( cgrupyqt.QtCore.QEvent.Clipboard)
+#	cgrupyqt.QtGui.QApplication.sendEvent( clipboard, event)
+	qapp.exec_()
 
