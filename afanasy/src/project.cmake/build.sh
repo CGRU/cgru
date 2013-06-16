16d49d95788dd69899443fff2ff9eaff75843aa1
timurhai <timurhai@gmail.com>
2013-06-16 21:55:43 +0400

    cmake extra libs can be specified in build script

--- afanasy/src/project.cmake/build.sh
+++ afanasy/src/project.cmake/build.sh
@@ -23,9 +23,10 @@ done
 # Configure SQL:
 export AF_POSTGRESQL=$sql
 
-# Configure build flags:
+# Configure building:
 export AF_ADD_CFLAGS=""
 export AF_ADD_LFLAGS="-lutil"
+export AF_EXTRA_LIBS=""
 
 echo "Building on '${DISTRIBUTIVE}'"
 case ${DISTRIBUTIVE} in
@@ -40,6 +41,7 @@ case ${DISTRIBUTIVE} in
     Ubuntu)
         export ADD_CMAKE_MODULE_PATH="$PWD"
 #        export AF_ADD_LFLAGS="$AF_ADD_LFLAGS -lpthread"
+        export AF_EXTRA_LIBS="pthread"
         ;;
     Mint)
         export ADD_CMAKE_MODULE_PATH="$PWD"
