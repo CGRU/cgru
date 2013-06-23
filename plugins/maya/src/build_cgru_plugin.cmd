367edf6c8e96abeff7bc57cb222e1a4261bbc084
timurhai <timurhai@gmail.com>
2013-06-23 02:58:32 -0700

    maya2014 mll windows ruby-mxx_ru

--- plugins/maya/src/build_cgru_plugin.cmd
+++ plugins/maya/src/build_cgru_plugin.cmd
@@ -1,5 +1,5 @@
-set MXX_RU_CPP_TOOLSET=vc9
-set VSINSTALLDIR="C:\Program Files (x86)\Microsoft Visual Studio 9.0"
+set MXX_RU_CPP_TOOLSET=vc10
+set VSINSTALLDIR="C:\Program Files (x86)\Microsoft Visual Studio 10.0"
 set VCVARS=amd64
 
 set MAYA_VERSION=2014
