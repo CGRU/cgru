5adca8ae90f60b4fdee3a3fc032cc04082c985c7
timurhai <timurhai@gmail.com>
2013-09-13 15:20:37 -0700

    win: maya setup

--- software_setup/setup_maya.cmd
+++ software_setup/setup_maya.cmd
@@ -25,8 +25,8 @@ set "MAYA_CGRU_MENUS_NAME=CGRU"
 rem Set custom values if needed here:
 IF EXIST override.cmd CALL override.cmd
 
-rem path to modeling, animation, rendering ... menu items
-rem if not sel defaut MAYA_CGRU_LOCATION/mel will be used
+rem path to modelling, animation, rendering ... menu items
+rem if not set default MAYA_CGRU_LOCATION/mel will be used
 rem "set MAYA_CGRU_MENUS_LOCATION=%MAYA_CGRU_LOCATION%\settings"
 set "MAYA_SCRIPT_PATH=%MAYA_CGRU_LOCATION%\mel\AETemplates;%MAYA_CGRU_LOCATION%\mel\_native\%MAYA_VERSION%;%MAYA_SCRIPT_PATH%"
 set "MAYA_PLUG_IN_PATH=%MAYA_CGRU_LOCATION%\mll\%MAYA_VERSION%;%MAYA_PLUG_IN_PATH%"
@@ -36,4 +36,4 @@ rem automatically load plugins located in MAYA_CGRU_LOCATION/mll/MAYA_VERSION di
 set "MAYA_CGRU_PLUG_INS_AUTOLOAD=1"
 
 rem Add Afanasy scripts:
-set "MAYA_SCRIPT_PATH=%CGRU_LOCATION%\afanasy;%MAYA_SCRIPT_PATH%"
+set "MAYA_SCRIPT_PATH=%MAYA_CGRU_LOCATION%\afanasy;%MAYA_SCRIPT_PATH%"
