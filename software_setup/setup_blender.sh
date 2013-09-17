a4837b95c57840497fcdb6ed400fa8d8282cb8ef
timurhai <timurhai@gmail.com>
2013-09-17 11:08:54 +0400

    blender: plugin moved to cgru

--- software_setup/setup_blender.sh
+++ software_setup/setup_blender.sh
@@ -10,14 +10,14 @@ export APP_DIR="${BLENDER_LOCATION}"
 export APP_EXE="${BLENDER_LOCATION}/blender"
 
 # Try to link Afanasy script to user addons:
-blender_home=$HOME/.blender
+blender_home=$HOME/.config/blender
 if [ -d "$blender_home" ]; then
    for ver in `ls "$blender_home"`; do
       addons="$blender_home/$ver/scripts/addons"
       [ -d "$addons" ] || mkdir -p "$addons"
       script="render_afanasy.py"
       link="$addons/$script"
-      [ -f "$link" ] || ln -svf "$AF_ROOT/plugins/blender/$script" "$link"
+      [ -f "$link" ] || ln -svf "$CGRU_LOCATION/plugins/blender/$script" "$link"
    done
 fi
 
