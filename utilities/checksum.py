bdd9510a6875503e58859c5905cabce72b320672
timurhai <timurhai@gmail.com>
2013-11-26 12:50:56 +0400

    utils: checksum: md5 version 2.0.0 started

--- utilities/checksum.py
+++ utilities/checksum.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 
-import json, os, subprocess, sys, time
+import json, hashlib, os, subprocess, sys, time
 import traceback
 
 from optparse import OptionParser
 Parser = OptionParser(usage="%prog [options] output\nType \"%prog -h\" for help", version="%prog 1.0")
 Parser.add_option('-i', '--input',   dest='input' ,  type  ='string',     default='',    help='Input file')
-Parser.add_option('-t', '--type',    dest='type'  ,  type  ='string',     default='md5', help='Input file')
 Parser.add_option('-o', '--output',  dest='output',  type  ='string',     default='',    help='Output file')
+Parser.add_option('-t', '--type',    dest='type',    type  ='string',     default='md5', help='Input file')
 Parser.add_option('-V', '--verbose', dest='verbose', action='store_true', default=False, help='Verbose mode')
 Parser.add_option('-D', '--debug',   dest='debug',   action='store_true', default=False, help='Debug mode')
 (Options, args) = Parser.parse_args()
@@ -43,13 +43,6 @@ if file_in_size == 0:
 	errorExit('Input file is empty:\n' + Options.input)
 
 
-cmd = None
-if Options.type == 'md5':
-	cmd = ['md5sum']
-else:
-	errorExit('Unsupported checksum type: "%s"' % Options.type)
-
-
 if Options.output == '':
 	Options.output = os.path.dirname( Options.input)
 	Options.output = os.path.join( Options.output, '.rules')
@@ -60,9 +53,17 @@ makeDir( os.path.dirname( Options.output))
 print('Input:  %s - %d bytes' % ( Options.input, file_in_size))
 print('Output: ' + Options.output)
 print('Type:   ' + Options.type)
+time_start = time.time()
+print('Started at: %s' % time.ctime( time_start))
 
 
-proc = subprocess.Popen( cmd, stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+chsum = None
+if Options.type == 'md5':
+	chsum = hashlib.md5()
+else:
+	errorExit('Unsupported checksum type: "%s"' % Options.type)
+
+
 file_in = open( Options.input, 'rb')
 read_len = 0
 progress = -1
@@ -70,7 +71,7 @@ while True:
 	data = file_in.read( 1000000)
 	if len( data) <= 0: break
 
-	proc.stdin.write( data)
+	chsum.update( data)
 
 	read_len += len(data)
 	new_progress = int(100.0 * read_len / file_in_size)
@@ -79,10 +80,8 @@ while True:
 		print('PROGRESS: %d%%' % progress)
 
 file_in.close()
-proc.stdin.close()
-result = proc.stdout.read().split(' ')[0]
+result = chsum.hexdigest()
 print( result)
-proc.wait()
 
 obj = dict()
 if os.path.isfile( Options.output):
@@ -92,7 +91,7 @@ if os.path.isfile( Options.output):
 	#print( json.dumps( obj))
 
 file_in_name = os.path.basename( Options.input)
-if not 'files' in obj: obj['files'] = dic()
+if not 'files' in obj: obj['files'] = dict()
 if not file_in_name in obj['files']: obj['files'][file_in_name] = dict()
 if not 'checksum' in obj['files'][file_in_name]: obj['files'][file_in_name]['checksum'] = dict()
 obj['files'][file_in_name]['checksum'][Options.type] = result
@@ -102,4 +101,16 @@ result = json.dumps( obj, indent=1)
 file_out = open( Options.output, 'w')
 file_out.write( result)
 file_out.close()
+
+# Output running time:
+time_finish = time.time()
+print('Finished at: %s' % time.ctime( time_finish))
+sec = time_finish - time_start
+hrs = int( sec / 3600 )
+sec -= hrs * 3600
+mns = int( sec / 60 )
+sec -= mns * 60
+msc = int( 1000.0 * sec - int( sec))
+sec = int( sec)
+print('Run time: %02d:%02d:%02d.%03d' % (hrs, mns, sec, msc))
 
