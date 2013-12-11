12f9c7bcfe255eb24594858b4691028dce5a04cb
timurhai <timurhai@gmail.com>
2013-12-11 11:08:07 -0800

    afanasy: c4d: parser: serach for subframe progess

--- afanasy/python/parsers/c4d.py
+++ afanasy/python/parsers/c4d.py
@@ -2,49 +2,51 @@ from parsers import parser
 
 import re
 
+re_percent = re.compile(r'Progress: \d+%')
 re_frame   = re.compile(r'Rendering frame \d+')
 re_number  = re.compile(r'\d+')
 Errors = ['Files cannot be written - please check output paths!','Error rendering project']
 
 class c4d(parser.parser):
-   'Cinema 4D'
-   def __init__( self):
-      parser.parser.__init__( self)
-      self.firstFrame = True
-      self.firstFrameNumber = 0
+	'Cinema 4D'
+	def __init__( self):
+		parser.parser.__init__( self)
+		self.firstFrame = True
+		self.firstFrameNumber = 0
 
-   def do( self, data, mode):
+	def do( self, data, mode):
+		need_calc = False
 
-    # Check if there are any Errors
-    for error in Errors:
-        if data.find(error) != -1:
-            self.error = True
-            break
+		# Check if there are any Errors
+		for error in Errors:
+			if data.find(error) != -1:
+				self.error = True
+				break
 
-    # Now update the some Informations like current frame & percent
-    match = re_frame.search( data )  
-    
-    if match == None: return
-    
-    # Get the current frame-number
-    frame = re_number.search( match.group(0))
-    thisFrame = int(frame.group(0))
-    
-    # To know with what frame the task starts
-    if self.firstFrame:
-        self.firstFrameNumber = thisFrame
-        self.firstFrame = False
-    
-    # Calculate the current frame (not the frame-number!)
-    currentFrame = thisFrame - self.firstFrameNumber
-    
-    # Calculate
-    taskPercent = int(float(currentFrame)/self.numframes*100)
-    
-    # It does not output anything about the frame progress it only displays when it is totally finished
-    self.percentframe = 100
-    
-    self.percent = taskPercent
-    self.frame = currentFrame  
-     
-    self.calculate()
+		# Search for frame percent:
+		match = re_percent.search( data )  
+		if match is not None:
+			frame = re_number.search( match.group(0))
+			self.percentframe = int(frame.group(0))
+			need_calc = True
+
+		# Search for frame number:
+		match = re_frame.search( data )  
+
+		if match is not None:
+			# Get the current frame-number
+			frame = re_number.search( match.group(0))
+			thisFrame = int(frame.group(0))
+
+			# To know with what frame the task starts
+			if self.firstFrame:
+				self.firstFrameNumber = thisFrame
+				self.firstFrame = False
+
+			# Calculate the current frame (not the frame-number!)
+			self.frame = thisFrame - self.firstFrameNumber
+
+			need_calc = True
+
+		if need_calc:
+			self.calculate()
