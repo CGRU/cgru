610c7d84e7648f6d5ee390b452a375d9d693fa62
timurhai <timurhai@vcentos-dev.(none)>
2014-02-18 22:30:41 +0400

    linux: maya: mll: no arch, just assuming that all is 64bit

--- plugins/maya/src/build_cgru_plugin.sh
+++ plugins/maya/src/build_cgru_plugin.sh
@@ -4,15 +4,14 @@ export UNIXTYPE="LINUX"
 export MXX_RU_CPP_TOOLSET=gcc_linux
 
 function build_cgru(){
-   if [ -z $MAYA_LOCATION ]; then
-      export MAYA_LOCATION=/usr/autodesk/maya$MAYA_VERSION$MAYA_ARCH
-   fi
-   echo "Building CGRU for Maya $MAYA_VERSION$MAYA_ARCH ..."
-   ruby cgru_plugin.mxw.rb $*
+	if [ -z $MAYA_LOCATION ]; then
+		export MAYA_LOCATION=/usr/autodesk/maya${MAYA_VERSION}-x64
+	fi
+	echo "Building CGRU for Maya $MAYA_VERSION ..."
+	ruby cgru_plugin.mxw.rb $*
 }
 
 export MAYA_VERSION=2014
-export MAYA_ARCH=-x64
 
 [ -f override.sh ] && source override.sh
 
