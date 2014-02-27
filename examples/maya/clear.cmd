b3e4b1d2283bc2fa3a6e50778ecccfb16edf25ee
timurhai <timurhai@gmail.com>
2014-02-27 12:53:49 -0800

    examples: maya: clrear script

--- examples/maya/clear.cmd
+++ examples/maya/clear.cmd
@@ -2,4 +2,5 @@ echo Clearing Maya examples in %CD%
 
 For /F "Tokens=*" %%I in ('dir /b "scene.mb*"') Do if not "%%I" == "scene.mb" del "%%I"
 
+if exist Keyboard rd /s /q Keyboard
 if exist render rd /s /q render
