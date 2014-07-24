594a0190dc77553a846ee3efffc4fa35a26625b1
timurhai <timurhai@gmail.com>
2014-07-24 20:04:38 +0400

    rules: statistics tables

--- rules/statistics.js
+++ rules/statistics.js
@@ -11,66 +11,53 @@ function stsc_Show( i_args)
 	if( i_elReports == null ) return;
 
 	i_elTasks.textContent = '';
+	var tasks = [];
+	var tasks_types = {};
 	var tasks_duration = 0;
-	var tasks_count = 0;
 
 	i_elReports.textContent = '';
+	var reports = [];
 	var reps_types = {};
 	var reps_duration = 0;
-	var reps_count = 0;
 
 	for( var s = 0; s < i_statuses.length; s++)
 	{
 		if( i_statuses[s] == null ) continue;
 
+		// Tasks collection:
 		if( i_statuses[s].tasks )
 		for( var t = 0; t < i_statuses[s].tasks.length; t++)
 		{
 			var task = i_statuses[s].tasks[t];
 
-			var el = document.createElement('div');
-			i_elTasks.appendChild( el);
-			el.classList.add('task');
-
-			var elDur = document.createElement('div');
-			el.appendChild( elDur);
-			elDur.classList.add('duration');
-			elDur.textContent = task.duration;
-
-			if( task.tags && task.tags.length )
-			{
-				var elTags = document.createElement('div');
-				el.appendChild( elTags);
-				elTags.classList.add('tags');
-
-				for( var g = 0; g < task.tags.length; g++)
-				{
-					var elTag = document.createElement('div');
-					elTags.appendChild( elTag);
-					elTag.classList.add('tag');
-					elTag.textContent = c_GetTagTitle( task.tags[g]);
-				}
-			}
-
-			if( task.artists && task.artists.length )
-			{
-				var elTags = document.createElement('div');
-				el.appendChild( elTags);
-				elTags.classList.add('artists');
-
-				for( var g = 0; g < task.artists.length; g++)
-				{
-					var elTag = document.createElement('div');
-					elTags.appendChild( elTag);
-					elTag.classList.add('tag');
-					elTag.textContent = c_GetUserTitle( task.artists[g]);
-				}
-			}
-
 			tasks_duration += task.duration;
-			tasks_count++;
+
+			if( i_statuses.length == 1 )
+			{
+				tasks_types[t] = task;
+				continue;
+			}
+
+			if(( task.tags == null ) || ( task.tags.length == 0 ))
+				task.tags = ['other'];
+
+			for( var g = 0; g < task.tags.length; g++)
+			{
+				var tag = task.tags[g];
+
+				if( tasks_types[tag] == null )
+					tasks_types[tag] = {"tags":[tag],"duration":0,"artists":[]};
+
+				if( task.artists && task.artists.length )
+				for( var a = 0; a < task.artists.length; a++)
+					if( tasks_types[tag].artists.indexOf( task.artists[a]) == -1 )
+						tasks_types[tag].artists.push( task.artists[a]);
+
+				tasks_types[tag].duration += task.duration;
+			}
 		}
 
+		// Reports collection:
 		if( i_statuses[s].reports )
 		for( var r = 0; r < i_statuses[s].reports.length; r++)
 		{
@@ -91,6 +78,7 @@ function stsc_Show( i_args)
 					rtype = {};
 					rtype.duration = 0;
 					rtype.artists = [];
+					rtype.tags = [report.tags[t]];
 					reps_types[report.tags[t]] = rtype;
 				}
 
@@ -102,43 +90,104 @@ function stsc_Show( i_args)
 			}
 
 			reps_duration += report.duration;
-			reps_count++;
 		}
 //console.log( JSON.stringify( report));
 	}
 
-	if( tasks_count )
+
+	// Tasks show:
+	for( var ttype in tasks_types ) tasks.push( tasks_types[ttype]);
+	if( tasks.length )
 	{
-		var el = document.createElement('div');
-		i_elTasks.appendChild( el);
-		el.textContent = 'Total Duration: ' + tasks_duration;
 		i_elTasksDiv.style.display = 'block';
+		stsc_ShowTable({"el":i_elTasks,"data":tasks,"total_duration":tasks_duration});
 	}
 	else
 		i_elTasksDiv.style.display = 'none';
 
-	for( var rtype in reps_types )
-	{
-		var el = document.createElement('div');
-		i_elReports.appendChild( el);
-		var info = c_GetTagTitle( rtype) + ': ' + reps_types[rtype].duration;
-		for( var a = 0; a < reps_types[rtype].artists.length; a++)
-		{
-			if( a ) info += ',';
-			info += ' ' + c_GetUserTitle( reps_types[rtype].artists[a]);
-		}
-		el.textContent = info;
-	}
 
-	if( reps_count )
+	// Reports show:
+	for( var rtype in reps_types ) reports.push( reps_types[rtype]);
+	if( reports.length )
 	{
-		var el = document.createElement('div');
-		i_elReports.appendChild( el);
-		el.textContent = 'Total Duration: ' + reps_duration;
 		i_elReportsDiv.style.display = 'block';
+		stsc_ShowTable({"el":i_elReports,"data":reports,"total_duration":reps_duration});
 	}
 	else
 		i_elReportsDiv.style.display = 'none';
 
+}
+
+function stsc_ShowTable( i_args)
+{
+	var i_el = i_args.el;
+	var i_data = i_args.data;
+	var i_total_duration = i_args.total_duration;
+
+	i_data.sort( function(a,b){if(a.duration<b.duration)return 1});
+
+	var elTb = document.createElement('table');
+	i_el.appendChild( elTb);
+	elTb.classList.add('statistics');
+
+	var elTr = document.createElement('tr');
+	elTb.appendChild( elTr);
+
+	var elTh = document.createElement('th');
+	elTr.appendChild( elTh);
+	elTh.textContent = 'D';
+	elTh.title = 'Duration';
+
+	var elTh = document.createElement('th');
+	elTr.appendChild( elTh);
+	elTh.textContent = 'Tags';
+	elTh.title = 'Tags';
+
+	var elTh = document.createElement('th');
+	elTr.appendChild( elTh);
+	elTh.textContent = 'Artists';
+	elTh.title = 'Artists';
+
+	for( var r = 0; r < i_data.length; r++)
+	{
+		var elTr = document.createElement('tr');
+		elTb.appendChild( elTr);
+
+		var elTd = document.createElement('td');
+		elTr.appendChild( elTd);
+		elTd.classList.add('duration');
+		elTd.textContent = i_data[r].duration;
+
+		var elTd = document.createElement('td');
+		elTr.appendChild( elTd);
+		var txt = '';
+		for( var t = 0; t < i_data[r].tags.length; t++)
+		{
+			if( t ) txt += ',';
+			txt += ' ' + c_GetTagTitle( i_data[r].tags[t]);
+		}
+		elTd.textContent = txt;
+
+		var elTd = document.createElement('td');
+		elTr.appendChild( elTd);
+		var txt = '';
+		for( var a = 0; a < i_data[r].artists.length; a++)
+		{
+			if( a ) txt += ',';
+			txt += ' ' + c_GetUserTitle( i_data[r].artists[a]);
+		}
+		elTd.textContent = txt;
+	}
+
+	var elTr = document.createElement('tr');
+	elTb.appendChild( elTr);
+
+	var elTd = document.createElement('th');
+	elTr.appendChild( elTd);
+	elTd.textContent = i_total_duration;
+
+	var elTd = document.createElement('th');
+	elTr.appendChild( elTd);
+	elTd.textContent = 'total';
 }
 
