55fa84934e88d46788b1f19d8d4011e475d5a685
timurhai <timurhai@gmail.com>
2014-08-06 22:03:13 +0400

    blender setup

--- software_setup/setup_blender.sh
+++ software_setup/setup_blender.sh
@@ -6,20 +6,12 @@ source "$CGRU_LOCATION/software_setup/setup__all.sh"
 # Setup default Blender location:
 export BLENDER_LOCATION="/usr/bin"
 
+export BLENDER_CGRU_PATH="${CGRU_LOCATION}/plugins/blender"
+
+export BLENDER_USER_SCRIPTS="${BLENDER_CGRU_PATH}"
+
 export APP_DIR="${BLENDER_LOCATION}"
 export APP_EXE="${BLENDER_LOCATION}/blender"
-
-# Try to link Afanasy script to user addons:
-blender_home=$HOME/.config/blender
-if [ -d "$blender_home" ]; then
-   for ver in `ls "$blender_home"`; do
-      addons="$blender_home/$ver/scripts/addons"
-      [ -d "$addons" ] || mkdir -p "$addons"
-      script="render_afanasy.py"
-      link="$addons/$script"
-      [ -f "$link" ] || ln -svf "$CGRU_LOCATION/plugins/blender/$script" "$link"
-   done
-fi
 
 # Define location:
 locate_file="$CGRU_LOCATION/software_setup/locate_blender.sh"
