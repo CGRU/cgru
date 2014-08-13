55ea84d95a865732d55ad299d695955c570b2a1d
timurhai <timurhai@gmail.com>
2014-08-13 20:30:54 +0400

    rules: archivate

--- utilities/arch.py
+++ utilities/arch.py
@@ -4,12 +4,14 @@
 import os
 import subprocess
 import sys
+import time
 
 
 from optparse import OptionParser
 Parser = OptionParser(usage="%prog [Options]\nType \"%prog -h\" for help", version="%prog 1.0")
 Parser.add_option('-i', '--input',   dest='input',    type  ='string',     default=None,  help='Input')
 Parser.add_option('-o', '--output',  dest='output',   type  ='string',     default=None,  help='Output')
+Parser.add_option('-t', '--type',    dest='type',     type  ='string',     default='zip', help='Type')
 Parser.add_option('-V', '--verbose', dest='verbose',  action='store_true', default=False, help='Verbose mode')
 Parser.add_option('-D', '--debug',   dest='debug',    action='store_true', default=False, help='Debug mode')
 Options, Args = Parser.parse_args()
@@ -35,25 +37,40 @@ if os.path.isdir( Options.input):
 	print('Size Total: %d' % SizeTotal)
 
 
-Cmd = ['7za','a','-y','-r','-t7z','-x!.*',Options.output,Options.input]
+if Options.output[-1-len(Options.type):] != '.'+Options.type:
+	Options.output += '.' + Options.type
+
+Cmd = None
+Key = None
+if Options.type == 'zip':
+	Cmd = ['zip','-y','-r','-1', Options.output, Options.input,'-x','*/.*']
+	Key = 'adding: '
+elif Options.type == '7z':
+	Cmd = ['7za','a','-y','-r','-t7z','-x!.*', Options.output, Options.input]
+	Key = 'Compressing '
+
+if Cmd is None:
+	errorExit('Unsupported arvhive type "%s"' % Options.type)
 
 print(' '.join(Cmd))
 
 if Options.debug:
 	sys.exit(0)
 
-Process = subprocess.Popen( Cmd, shell=False, stdout=subprocess.PIPE)
+if os.path.isfile(Options.output):
+	print('Deleting exising archive:\n' + Options.output)
+	os.remove(Options.output)
+
+Process = subprocess.Popen( Cmd, shell=False, bufsize=1, stdout=subprocess.PIPE)
 
 while True:
-	Process.stdout.flush()
 	data = Process.stdout.readline()
 	if data is None: break
 	if len(data) < 1: break
 
 	sys.stdout.write(data)
 
-	if data.find('Compressing ') != -1 and FilesTotal:
-		data = data[12:].strip()
+	if data.find(Key) != -1 and FilesTotal:
 		if data not in Files:
 			print('PROGRESS: %d%%' % int( 100.0 * len(Files) / FilesTotal))
 			Files.append( data)
