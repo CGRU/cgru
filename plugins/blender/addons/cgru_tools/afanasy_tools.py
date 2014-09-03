91816665873dc96876ad12d152af11a92e5d06ab
mifth <paulgeraskin@gmail.com>
2014-09-03 18:54:37 +0400

    Cleanup blender plugin.

--- plugins/blender/addons/cgru_tools/afanasy_tools.py
+++ plugins/blender/addons/cgru_tools/afanasy_tools.py
@@ -4,8 +4,6 @@ import shutil
 import time
 import os
 import sys
-#sys.path.append( '/home/mifth/Documents/repositories/cgru/afanasy/python' )
-#sys.path.append( '/home/mifth/Documents/repositories/cgru/lib/python' )
 
 import bpy
 
@@ -13,120 +11,21 @@ from bpy.props import (PointerProperty, StringProperty, BoolProperty,
 					   EnumProperty, IntProperty, CollectionProperty)
 from bpy.types import Operator, AddonPreferences
 
-# bpy.CURRENT_VERSION = bl_info["version"][0]
-# bpy.found_newer_version = False
-#bpy.up_to_date = False
-#bpy.download_location = 'http://www.sourceforge.net/projects/cgru'
-
 bpy.errors = []
-
-
-#def renderEngine(render_engine):
-	#"""Missing DocString
-
-	#:param render_engine:
-	#:return:
-	#"""
-	#bpy.utils.register_class(render_engine)
-	#return render_engine
-
-
-#def getListRenderers(scene, context):
-	#lst = [("BLENDER_RENDER","BLENDER_RENDER",""), ("CYCLES","CYCLES","")]
-
-	#prefs = context.user_preferences.addons[__name__].preferences
-	#if prefs.customEngine != '':
-		#renderersSplit = prefs.customEngine.split(",")
-		#for customRen in renderersSplit:
-			#lst.append((customRen, customRen,""))
-
-	#return lst
 
 class OREAddonPreferences(AddonPreferences):
 	# this must match the addon name, use '__name__'
 	# when defining this in a submodule of a python package.
-	# bl_idname = __name__
 	bl_idname = __name__
-
-	#customEngine = StringProperty(name='Custom Engines',
-							#description='''Custom Engines. Use "," to split renderers''',
-							#maxlen=512, default='')
-
-	#engineList = EnumProperty(name='Use Engine',
-							#description='Engine to render scene with.',
-							#items=getListRenderers)
 
 	def draw(self, context):
 		layout = self.layout
 		row = layout.row()
 		row.label(text="Please, set Exchanges Folder and save Preferences")
 		row = layout.row()
-		#row.prop(self, "engineList")
-		#layout.prop(self, "customEngine")
-		#col.operator("scene.ms_add_lightmap_group", icon='ZOOMIN', text="")
-		#col.operator("scene.ms_del_lightmap_group", icon='ZOOMOUT', text="")
 
 
-
-## all panels, except render panel
-## Example of wrapping every class 'as is'
-#from bl_ui import properties_scene
-
-#for member in dir(properties_scene):
-	#subclass = getattr(properties_scene, member)
-	#try:
-		#subclass.COMPAT_ENGINES.add('AFANASY_RENDER')
-	#except:  # TODO: Too broad exception clause
-		#pass
-
-#del properties_scene
-
-#from bl_ui import properties_world
-
-#for member in dir(properties_world):
-	#subclass = getattr(properties_world, member)
-	#try:
-		#subclass.COMPAT_ENGINES.add('AFANASY_RENDER')
-	#except:  # TODO: Too broad exception clause
-		#pass
-
-#del properties_world
-
-#from bl_ui import properties_material
-
-#for member in dir(properties_material):
-	#subclass = getattr(properties_material, member)
-	#try:
-		#subclass.COMPAT_ENGINES.add('AFANASY_RENDER')
-	#except:  # TODO: Too broad exception clause
-		#pass
-
-#del properties_material
-
-#from bl_ui import properties_object
-
-#for member in dir(properties_object):
-	#subclass = getattr(properties_object, member)
-	#try:
-		#subclass.COMPAT_ENGINES.add('AFANASY_RENDER')
-	#except:  # TODO: Too broad exception clause
-		#pass
-
-#del properties_object
-
-
-#class RenderButtonsPanel():
-	#"""Missing DocString
-	#"""
-	#bl_space_type = 'PROPERTIES'
-	#bl_region_type = 'WINDOW'
-	#bl_context = "render"
-
-
-## COMPAT_ENGINES must be defined in each subclass, external engines can add
-## themselves here
-
-
+# This method creates a string with a list of engines in all scenes
 def getSceneEngines():
 	str = ''
 	strEngines = []
@@ -149,24 +48,14 @@ class RENDER_PT_Afanasy(bpy.types.Panel):
 	bl_category = 'CGRU'
 	#bl_options = {'DEFAULT_CLOSED'}
 
-	#@classmethod
-	#def poll(cls, context):
-		#rd = context.scene.render
-		#return (rd.use_game_engine is False) \
-			   #and (rd.engine in cls.COMPAT_ENGINES)
-
 	def draw(self, context):
 		layout = self.layout
 		sce = context.scene
 		ore = sce.ore_render
 
 		layout.prop(ore, 'jobname')
-
-		#user_preferences = context.user_preferences
-		#addon_prefs = user_preferences.addons[__name__].preferences
 		layout.label(text="Engines: " + getSceneEngines())
-		#layout.operator('ore.setrenderer')
-		#layout.prop(context.user_preferences.addons[__name__].preferences, 'engineList', text='Render Engine')
+
 
 		layout.separator()
 		row = layout.row()
@@ -186,9 +75,6 @@ class RENDER_PT_Afanasy(bpy.types.Panel):
 		layout.prop(ore, 'relativePaths')
 		layout.prop(ore, 'packTextures')
 
-		# layout.separator()
-		# layout.operator('ore.docs', icon='INFO')
-
 
 class PARAMETERS_PT_RenderSettings(bpy.types.Panel):
 	"""Missing DocString
@@ -199,12 +85,6 @@ class PARAMETERS_PT_RenderSettings(bpy.types.Panel):
 	bl_region_type = 'TOOLS'
 	bl_context = "objectmode"
 	bl_category = 'CGRU'
-
-	#@classmethod
-	#def poll(cls, context):
-		#rd = context.scene.render
-		#return (rd.use_game_engine == False) and (
-			#rd.engine in cls.COMPAT_ENGINES)
 
 	def draw(self, context):
 		layout = self.layout
@@ -222,12 +102,6 @@ class PARAMETERS_PT_Afanasy(bpy.types.Panel):
 	bl_context = "objectmode"
 	bl_category = 'CGRU'
 
-	#@classmethod
-	#def poll(cls, context):
-		#rd = context.scene.render
-		#return (rd.use_game_engine == False) and (
-			#rd.engine in cls.COMPAT_ENGINES)
-
 	def draw(self, context):
 		layout = self.layout
 		ore = context.scene.ore_render
@@ -237,27 +111,6 @@ class PARAMETERS_PT_Afanasy(bpy.types.Panel):
 		layout.prop(ore, 'dependmaskglobal')
 		layout.prop(ore, 'hostsmask')
 		layout.prop(ore, 'hostsmaskexclude')
-
-
-#class ORE_SetRenderer(bpy.types.Operator):
-	#"""Missing DocString
-	#"""
-
-	#bl_idname = "ore.setrenderer"
-	#bl_label = "Set Renderer"
-	#bl_space_type = 'VIEW_3D'
-	#bl_region_type = 'TOOLS'
-	#bl_context = "objectmode"
-	#bl_category = 'CGRU'
-
-	#def execute(self, context):
-		#sce = context.scene
-		#ore = sce.ore_render
-		#prefs = context.user_preferences.addons[__name__].preferences
-
-		##ore.engine = prefs.engineList
-
-		#return set(['FINISHED'])
 
 
 class ORE_Submit(bpy.types.Operator):
@@ -351,9 +204,11 @@ class ORE_Submit(bpy.types.Operator):
 
 		#if enginesString == 'BLENDER_RENDER':
 			#block.setParser('blender_render')
-
 		#if enginesString == 'CYCLES':
 			#block.setParser('blender_cycles')
+
+		# set blender_cycles parser for all engines
+		block.setParser('blender_cycles')
 
 		job.blocks.append(block)
 		# Set block command and frame range:
@@ -403,19 +258,6 @@ class ORE_Submit(bpy.types.Operator):
 		bpy.ops.wm.open_mainfile(filepath=scenefile)
 
 		return set(['FINISHED'])
-
-
-#class Afanasy(bpy.types.Panel):
-	#bl_idname = 'AFANASY_RENDER'
-	#bl_label = "Afanasy"
-
-	#def render(self, scene):
-		#"""Missing DocString
-
-		#:param scene:
-		#:return:
-		#"""
-		#print('Render with Afanasy farm manager')
 
 
 def register():
