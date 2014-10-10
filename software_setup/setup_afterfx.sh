a3821ec4f0eb279192d19a2c594668acddb95882
Sylvain Maziere <sylvain@predat.fr>
2014-10-10 14:19:42 +0200

    afterfx cc 2014 for MacOSX

--- software_setup/setup_afterfx.sh
+++ software_setup/setup_afterfx.sh
@@ -3,13 +3,13 @@
 # Source general for all soft directives:
 source "$CGRU_LOCATION/software_setup/setup__all.sh"
 
-export AE_LOCATION="/Applications/Adobe After Effects CS5"
+export AE_LOCATION="/Applications/Adobe After Effects CC 2014"
 
 export APP_DIR=$AE_LOCATION
 
 # One is for the AfterEffects itself and the other one for the renderer
 export APP_EXE_RENDER="$AE_LOCATION/aerender"
-export APP_EXE="$AE_LOCATION/Adobe After Effects CS5.app/Contents/MacOS/After Effects"
+export APP_EXE="$AE_LOCATION/Adobe After Effects CC 2014.app/Contents/MacOS/After Effects"
 
 # Define location:
 export locate_file=$CGRU_LOCATION/software_setup/locate_afterfx.sh
