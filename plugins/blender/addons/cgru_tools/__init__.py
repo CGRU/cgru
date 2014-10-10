4ca3e9e0f9308ceb4e564b45268f5bccfe0be985
Sylvain Maziere <sylvain@predat.fr>
2014-10-10 11:03:05 +0200

    blender addons: clean up indentation and typo.

--- plugins/blender/addons/cgru_tools/__init__.py
+++ plugins/blender/addons/cgru_tools/__init__.py
@@ -28,49 +28,44 @@ class ORESettings(bpy.types.PropertyGroup):
 	"""
 	# General:
 	jobname = StringProperty(name='Job Name',
-							 description='Job Name. Scene name if empty.',
-							 maxlen=512, default='')
-
+		description='Job Name. Scene name if empty.', maxlen=512, default='')
 	fpertask = IntProperty(name='Per Task', description='Frames Per One Task',
-						   min=1, default=1)
+		min=1, default=1)
 	pause = BoolProperty(name='Start Job Paused',
-						 description='Send job in offline state.', default=0)
-
+		description='Send job in offline state.', default=0)
 	packLinkedObjects = BoolProperty(name='Pack Linked Objects',
-						 description='Make Loacal All linked Groups and Objects', default=0)
+		description='Make Local All linked Groups and Objects', default=0)
 	relativePaths = BoolProperty(name='Relative Paths',
-						 description='Set Relative Paths for all Textures and Objects', default=0)
+		description='Set Relative Paths for all Textures and Objects', default=0)
 	packTextures = BoolProperty(name='Pack Textures',
-						 description='Pack all Textures into the Blend File.', default=0)
+		description='Pack all Textures into the Blend File.', default=0)
 
 	# Render Settings:
 	filepath = StringProperty(name='File Path', description='Set File Path.',
-							  maxlen=512, default='')
+		maxlen=512, default='')
 
 	# Paramerets:
 	priority = IntProperty(name='Priority',
-						   description='Job order in user jobs list.', min=-1,
-						   max=250, default=-1)
+		description='Job order in user jobs list.', min=-1,
+		max=250, default=-1)
 	maxruntasks = IntProperty(name='Max Run Tasks',
-							  description='Maximum number of running tasks.',
-							  min=-1, max=9999, default=-1)
+		description='Maximum number of running tasks.',
+		min=-1, max=9999, default=-1)
 	dependmask = StringProperty(name='Depend Mask',
-								description='Jobs to wait pattern.',
-								maxlen=512, default='')
+		description='Jobs to wait pattern.',
+		maxlen=512, default='')
 	dependmaskglobal = StringProperty(name='Global Depend',
-									  description='All users jobs wait pattern.',
-									  maxlen=512, default='')
+		description='All users jobs wait pattern.',
+		maxlen=512, default='')
 	hostsmask = StringProperty(name='Hosts Mask',
-							   description='Hosts to run pattern.', maxlen=512,
-							   default='')
+		description='Hosts to run pattern.', maxlen=512,
+		default='')
 	hostsmaskexclude = StringProperty(name='Exclude Hosts',
-									  description='Hosts to ignore pattern.',
-									  maxlen=512, default='')
+		description='Hosts to ignore pattern.',
+		maxlen=512, default='')
 
 
 def register():
-	bpy.CGRUTools = dict()
-
 	bpy.utils.register_module(__name__)
 	bpy.types.Scene.ore_render = PointerProperty(
 		type=ORESettings,
@@ -80,12 +75,10 @@ def register():
 
 
 def unregister():
-	import bpy
-
 	del bpy.types.Scene.ore_render
-	del bpy.CGRUTools
 	bpy.utils.unregister_module(__name__)
 
 
 if __name__ == "__main__":
 	register()
+
