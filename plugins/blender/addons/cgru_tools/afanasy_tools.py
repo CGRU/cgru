30c083bfde1c739d29c8ef543062f170d15c0e1b
jasperge <jaspervannieuwenhuizen@gmail.com>
2015-03-10 12:16:57 +0100

    Changed layout of Afanasy panel

--- plugins/blender/addons/cgru_tools/afanasy_tools.py
+++ plugins/blender/addons/cgru_tools/afanasy_tools.py
@@ -63,18 +63,16 @@ class RENDER_PT_Afanasy(bpy.types.Panel):
 		col.prop(ore, 'packTextures')
 
 		layout.separator()
-		col = layout.column(align=True)
-		row = col.row(align=True)
-		row.scale_y = 1.4
-		row.prop(sce, 'frame_start')
-		row.prop(sce, 'frame_end')
-		row.prop(sce, 'frame_step')
-		row.prop(ore, 'fpertask')
-		row = col.row(align=True)
-		row.scale_y = 1.2
-		row.prop(ore, 'priority')
-		row.prop(ore, 'maxruntasks')
-		row.prop(ore, 'sequential')
+		row = layout.row(align=False)
+		col = row.column(align=True)
+		col.prop(sce, 'frame_start')
+		col.prop(sce, 'frame_end')
+		col.prop(sce, 'frame_step')
+		col = row.column(align=True)
+		col.prop(ore, 'fpertask')
+		col.prop(ore, 'sequential')
+		col.prop(ore, 'priority')
+		col.prop(ore, 'maxruntasks')
 
 		layout.separator()
 		col = layout.column()
