74339964c99b96f7e80d14657accc3e6e30e118c
timurhai <timurhai@gmail.com>
2015-04-24 18:30:51 +0300

    rules: *.ogv is a video

--- rules/common.js
+++ rules/common.js
@@ -3,7 +3,7 @@ RULES = {};
 RULES.rufolder = 'rules';
 RULES_TOP = {};
 
-c_movieTypes = ['mpg','mpeg','mov','avi','mp4','m4v','ogg','mxf','flv'];
+c_movieTypes = ['mpg','mpeg','mov','avi','mp4','m4v','ogg','ogv','mxf','flv'];
 c_movieTypesHTML = ['mp4','ogg'];
 c_imageTypes = ['jpg','jpeg','png','exr','dpx','tga','tif','tiff','psd','xcf'];
 c_imageEditableTypes = ['jpg','jpeg','png'];
