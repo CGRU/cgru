58d17030b47cb645c1e87e1e8b3b63ea7c7d26a1
timurhai <timurhai@gmail.com>
2015-10-04 13:10:42 -0700

    fusion

--- software_setup/setup_fusion.cmd
+++ software_setup/setup_fusion.cmd
@@ -3,11 +3,17 @@ call %CGRU_LOCATION%\software_setup\setup__all.cmd
 
 rem Setup CGRU common scripts if location provided
 set "FUSION_CGRU_PATH=%CGRU_LOCATION%\plugins\fusion"
+set "FUSION_CGRU_PREFS=%FUSION_CGRU_PATH%\cgru.prefs"
 if defined FUSION_MasterPrefs (
-	set "FUSION_MasterPrefs=%FUSION_CGRU_PATH%\cgru.prefs;%FUSION_MasterPrefs%"
+	set "FUSION_MasterPrefs=%FUSION_CGRU_PREFS%;%FUSION_MasterPrefs%"
 ) else (
-	set "FUSION_MasterPrefs=%FUSION_CGRU_PATH%\cgru.prefs"
+	set "FUSION_MasterPrefs=%FUSION_CGRU_PREFS%"
 )
+
+if not exist %FUSION_CGRU_PREFS% (
+	"%CGRU_PYTHONEXE%" "%FUSION_CGRU_PATH%"\makeprefs.py
+)
+rem {Global={Paths={Map={["Scripts:"]="C:\\cgru\\plugins\\fusion\\;Fusion:Scripts",},},},}
 
 SET "APP_DIR=C:\Program Files\Blackmagic Design\Fusion"
 SET "APP_EXE=%APP_DIR%\Fusion.exe"
