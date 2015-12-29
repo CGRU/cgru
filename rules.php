14ba0178fcc35fcbca652635225de341b82cf7af
timurhai <timurhai@gmail.com>
2015-12-29 23:00:34 +0300

    rules apache log warning fix

--- rules.php
+++ rules.php
@@ -151,17 +151,17 @@ function jsf_start( $i_arg, &$o_out)
 function get_client_ip()
 {
 	$ipaddress = '';
-	if ($_SERVER['HTTP_CLIENT_IP'])
+	if( isset($_SERVER['HTTP_CLIENT_IP']))
 		$ipaddress = $_SERVER['HTTP_CLIENT_IP'];
-	else if($_SERVER['HTTP_X_FORWARDED_FOR'])
+	else if( isset($_SERVER['HTTP_X_FORWARDED_FOR']))
 		$ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
-	else if($_SERVER['HTTP_X_FORWARDED'])
+	else if( isset($_SERVER['HTTP_X_FORWARDED']))
 		$ipaddress = $_SERVER['HTTP_X_FORWARDED'];
-	else if($_SERVER['HTTP_FORWARDED_FOR'])
+	else if( isset($_SERVER['HTTP_FORWARDED_FOR']))
 		$ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
-	else if($_SERVER['HTTP_FORWARDED'])
+	else if( isset($_SERVER['HTTP_FORWARDED']))
 		$ipaddress = $_SERVER['HTTP_FORWARDED'];
-	else if($_SERVER['REMOTE_ADDR'])
+	else if( isset($_SERVER['REMOTE_ADDR']))
 		$ipaddress = $_SERVER['REMOTE_ADDR'];
 	else
 		$ipaddress = 'UNKNOWN';
