c3aa7129d8f529138bdcebb60ce7fceb0d3e3fb9
timurhai <timurhai@gmail.com>
2016-05-23 17:10:37 +0300

    rules: export shots table to a sigle html file (to print, send)

--- rules/table.js
+++ rules/table.js
@@ -5,8 +5,8 @@ table_columns.state    = {'label':'State'};
 table_columns.info     = {'label':'Info'};
 table_columns.comments = {'label':'Comments'};
 table_columns.tasks    = {'label':'Tasks'};
-table_columns.duration = {'label':'D'};
-table_columns.price    = {'label':'P'};
+table_columns.duration = {'label':'Dur'};
+table_columns.price    = {'label':'Price'};
 
 table_args = null;
 table_doc = null;
@@ -16,32 +16,80 @@ table_shot = null;
 table_functions = null;
 table_function_num = null;
 
+table_params = {};
+table_params.picture  = {'type':'bool','default':true,'width':'20%'};
+table_params.state    = {'type':'bool','default':true,'width':'20%'};
+table_params.tasks    = {'type':'bool','default':true,'width':'20%'};
+table_params.duration = {'type':'bool','default':true,'width':'20%'};
+table_params.price    = {'type':'bool','default':true,'width':'20%'};
+
 function table_Export( i_args)
+{
+	var wnd = new cgru_Window({'name':'shots_table_export','title':'Export Shots Table'});
+	wnd.m_args = i_args;
+
+	gui_Create( wnd.elContent, table_params, []);
+
+	var elExport = document.createElement('div');
+	wnd.elContent.appendChild( elExport);
+	elExport.textContent = 'Export';
+	elExport.classList.add('button');
+	elExport.m_wnd = wnd;
+	elExport.onclick = function(e){ table_ExportDo( e.currentTarget.m_wnd);}
+}
+
+function table_ExportDo( i_wnd)
+{
+	args = i_wnd.m_args;
+	args.params = gui_GetParams( i_wnd.elContent, table_params);
+	table_Start( args);
+}
+
+function table_Start( i_args)
 {
 	//console.log( JSON.stringify( i_args));
 	//console.log( i_args);
-
-	// Arguments:
-	table_args = i_args;
-	table_shots = i_args.shots;
-
-	// Prepare input data:
-	for( var s = 0; s < table_shots.length; s++)
-	if( table_shots[s].status == null )
-		table_shots[s].status = {};
 
 	// Initializations:
 	table_shot_num = 0;
 	table_function_num = -1;
 	table_functions = [];
+
+
+	// Arguments:
+	table_args = i_args;
+	table_shots = i_args.shots;
+	var table_show = {};
 	for( var c in table_columns )
-		table_functions.push( window['table_Gen_' + c]);
+	{
+		table_show[c] = true;
+		if( i_args.params[c] === false )
+			table_show[c] = false;
+	}
+
+
+	// Prepare input data:
+	for( var s = 0; s < table_shots.length; s++)
+	{
+		var shot = table_shots[s];
+
+		if( shot.status == null )
+			shot.status = {};
+
+		if( shot.status.flags ==  null )
+			shot.status.flags = [];
+
+		if( shot.status.flags.indexOf('omit') != -1 )
+			shot.omit = true;
+	}
+
 
 	// Prepare document title:
 	var title = g_CurPath();
 	title = title.replace(/^\//,'');
 	title = title.replace(/\/$/,'');
 	title = title.replace(/\//g,'_');
+
 
 	// Open new window
 	var wnd = window.open( null, title/*, 'location=no,scrollbars=yes,resizable=yes'/*,menubar=no'*/);
@@ -51,6 +99,7 @@ function table_Export( i_args)
 		return;
 	}
 	table_doc = wnd.document;
+
 
 	// Write headers:
 	var lines = ['<!DOCTYPE html>',
@@ -65,21 +114,71 @@ function table_Export( i_args)
 			'page-break-inside:avoid;',
 			'height: 50px;',
 			'}',
-		'td {',
+		'td, th {',
 			'text-align: center;',
 			'border: 1px solid rgba(0,0,0,.5);',
 			'padding: 4px;',
 			'overflow: hidden;',
 			'height: 50px;',
 			'}',
-		'</style>',
-		'<table>'];
+		'</style>'];
+	table_WriteLines( lines);
+	table_Write('<h3>' + title + '</h3>');
+
+
+	// Summary:
+	var progress = 0;
+	var omits = 0;
+	var duration = 0;
+	var price = 0;
+	for( var s = 0; s < table_shots.length; s++)
+	{
+		var shot = table_shots[s];
+
+		if( shot.omit )
+		{
+			omits ++;
+			continue;
+		}
+
+		if( shot.status.progress && ( shot.status.progress > 0 ))
+			progress += shot.status.progress;
+
+		if( shot.status.duration && ( shot.status.duration > 0 ))
+			duration += shot.status.duration;
+
+		if( shot.status.price && ( shot.status.price > 0 ))
+			price += shot.status.price;
+	}
+	progress = Math.floor( progress / ( table_shots.length - omits ));
+	if( price    == 0 ) table_show.price = false;
+	if( duration == 0 ) table_show.duration = false;
+
+	var lines = ['<table>',
+		'<tr>',
+			'<th>Count</th>',
+			'<th>Progress</th>',
+			'<th>Duration</th>',
+			'<th>Price</th>',
+		'</tr>',
+		'<tr>',
+			'<td>' + ( table_shots.length - omits ) + (omits?(' ( +' + omits + ' omits)'):'') + '</td>',
+			'<td>' + progress + '%</td>',
+			'<td>' + duration + '</td>',
+			'<td>' + price + '</td>',
+		'</tr>',
+		'</table>'];
 	table_WriteLines( lines);
 
+
 	// Write shots table caption:
+	table_Write('<table>');
 	ln = '<tr>';
 	for( c in table_columns )
 	{
+		if( false == table_show[c] )
+			continue;
+
 		ln += '<th>';
 		ln += table_columns[c].label;
 		ln += '</th>';
@@ -87,8 +186,16 @@ function table_Export( i_args)
 	ln += '</tr>';
 	table_Write( ln);
 
+
 	// Start write shots table:
 	table_Write('<tr>');
+	for( var c in table_columns )
+	{
+		if( false == table_show[c] )
+			continue;
+
+		table_functions.push( window['table_Gen_' + c]);
+	}
 	table_Function();
 }
 
@@ -153,13 +260,18 @@ function table_Gen_picture( i_shot)
 	xhr.responseType = 'blob';
 	xhr.onload = function()
 	{
+		if( xhr.status != 200 )
+		{
+			table_PictureReceived( null);
+			return;
+		}
+
 		var reader = new FileReader();
 		reader.onloadend = function()
 		{
 			table_PictureReceived( reader.result);
 		}
 		reader.readAsDataURL( xhr.response);
-		xhr.response;
 	}
 	xhr.open('GET', url);
 	xhr.send();
@@ -167,7 +279,10 @@ function table_Gen_picture( i_shot)
 function table_PictureReceived( i_data)
 {
 	//console.log( i_data);
-	var img = '<img src="' + i_data + '" width="200px">';
+	var img = '';
+
+	if( i_data )
+		img = '<img width="200px" src="' + i_data + '">';
 
 	table_Write( img, true);
 
@@ -199,8 +314,10 @@ function table_BodyReceived( i_data, i_args)
 
 	if( i_data.indexOf('"error"') == -1 )
 	{
+		var body = i_data.replace(/<(?:.|\n)*?>/gm, '');
+
 		info += '<div style="text-align: left">'
-		info += i_data.replace(/\<\s*br\s*\/?\s*\>/g,' ');
+		info += body;
 		info += '</div>'
 	}
 
@@ -211,9 +328,32 @@ function table_BodyReceived( i_data, i_args)
 
 function table_Gen_comments( i_shot)
 {
-	var cm = '';
+	var cpath = c_GetRuFilePath( cm_file, i_shot.path);
+	n_GetFile({'path':cpath,'func':table_CommentsReceived,'info':'table_comments','parse':true});
+}
+function table_CommentsReceived( i_data, i_args)
+{
+	var text = '';
 
-	table_Write( cm, true);
+	if(( i_data.error == null ) && ( i_data.comments))
+	{
+		var cm_array = [];
+		for( key in i_data.comments )
+		{
+			i_data.comments[key].key = key;
+			cm_array.push( i_data.comments[key]);
+		}
+
+		if( cm_array.length )
+		{
+			cm_array.sort( function(a,b){if(a.key<b.key)return -1;if(a.key>b.key)return 1;return 0;});
+			text = cm_array[0].text;
+			text = text.replace(/<(?:.|\n)*?>/gm, '');
+		}
+	}
+
+//text = JSON.stringify( i_data);
+	table_Write( text, true);
 
 	table_Function();
 }
