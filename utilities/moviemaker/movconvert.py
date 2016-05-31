f5e51bab7376083aaa1f23d86976355027403069
timurhai <timurhai@gmail.com>
2016-05-31 19:16:23 +0300

    movconvert: if 'error' string founded, output bad result for parser

--- utilities/moviemaker/movconvert.py
+++ utilities/moviemaker/movconvert.py
@@ -9,8 +9,8 @@ import time
 from optparse import OptionParser
 
 Parser = OptionParser(
-	usage="%prog [options] input\ntype \"%prog -h\" for help",
-	version="%prog 1.  0"
+    usage="%prog [options] input\ntype \"%prog -h\" for help",
+    version="%prog 1.  0"
 )
 
 Parser.add_option('-a', '--avcmd',     dest='avcmd',     type  ='string', default='ffmpeg', help='AV convert command')
@@ -30,43 +30,43 @@ Parser.add_option(      '--imgname',   dest='imgname',   type  ='string', defaul
 Options, argv = Parser.parse_args()
 
 if len(argv) < 1:
-	print('ERROR: Movie file not specified.')
-	sys.exit(0)
+    print('ERROR: Movie file not specified.')
+    sys.exit(0)
 
 Input = argv[0]
 Output = Options.output
 if Output == '':
-	Output = Input
+    Output = Input
 
 Sequence = []
 StartNumber = None
 if os.path.isdir(Input):
-	InDir = Input
-	allfiles = os.listdir(InDir)
-	allfiles.sort()
-	for afile in allfiles:
-		if afile[0] == '.': continue
-		afile = os.path.join( InDir, afile)
-		if os.path.isdir( afile): continue
+    InDir = Input
+    allfiles = os.listdir(InDir)
+    allfiles.sort()
+    for afile in allfiles:
+        if afile[0] == '.': continue
+        afile = os.path.join( InDir, afile)
+        if os.path.isdir( afile): continue
 
-		Sequence.append( afile)
+        Sequence.append( afile)
 
-		if StartNumber is not None: continue
+        if StartNumber is not None: continue
 
-		afile = os.path.basename( afile)
-		digits = re.findall(r'\d+', afile)
-		if len(digits) == 0: continue
-		digits = digits[-1]
-		StartNumber = int(digits)
-		Input = afile[:afile.rfind(digits)]
-		Input += '%0' + str(len(digits)) + 'd'
-		Input += afile[afile.rfind(digits) + len(digits):]
-		Input = os.path.join(argv[0], Input)
-		continue
+        afile = os.path.basename( afile)
+        digits = re.findall(r'\d+', afile)
+        if len(digits) == 0: continue
+        digits = digits[-1]
+        StartNumber = int(digits)
+        Input = afile[:afile.rfind(digits)]
+        Input += '%0' + str(len(digits)) + 'd'
+        Input += afile[afile.rfind(digits) + len(digits):]
+        Input = os.path.join(argv[0], Input)
+        continue
 else:
-	if not os.path.isfile(Input):
-		print('ERROR: Input does not exist: ' + Input)
-		sys.exit(1)
+    if not os.path.isfile(Input):
+        print('ERROR: Input does not exist: ' + Input)
+        sys.exit(1)
 
 MOVIEMAKER = os.path.dirname(sys.argv[0])
 CODECSDIR = os.path.join(MOVIEMAKER, 'codecs')
@@ -74,122 +74,122 @@ CODECSDIR = os.path.join(MOVIEMAKER, 'codecs')
 Codec = Options.codec
 
 if Codec is None:
-	args = [Options.avcmd, '-y']
-	if Options.timestart != '':
-		args.extend(['-ss', Options.timestart])
-	args.extend(['-i', Input])
-	if Options.duration != '':
-		args.extend(['-t', Options.duration])
-	args.extend(['-an', '-f', 'image2'])
-	if Options.type == 'jpg':
-		args.extend(['-qscale', str(Options.qscale)])
-		Output += '.q' + str(Options.qscale)
-	elif Options.type == 'dpx':
-		args.extend(['-pix_fmt','gbrp10be'])
-	elif Options.type == 'tif' or Options.type == 'tif8':
-		args.extend(['-pix_fmt','rgb24'])
-		Options.type = 'tif'
-	elif Options.type == 'tif16':
-		args.extend(['-pix_fmt','rgb48le'])
-		Options.type = 'tif'
+    args = [Options.avcmd, '-y']
+    if Options.timestart != '':
+        args.extend(['-ss', Options.timestart])
+    args.extend(['-i', Input])
+    if Options.duration != '':
+        args.extend(['-t', Options.duration])
+    args.extend(['-an', '-f', 'image2'])
+    if Options.type == 'jpg':
+        args.extend(['-qscale', str(Options.qscale)])
+        Output += '.q' + str(Options.qscale)
+    elif Options.type == 'dpx':
+        args.extend(['-pix_fmt','gbrp10be'])
+    elif Options.type == 'tif' or Options.type == 'tif8':
+        args.extend(['-pix_fmt','rgb24'])
+        Options.type = 'tif'
+    elif Options.type == 'tif16':
+        args.extend(['-pix_fmt','rgb48le'])
+        Options.type = 'tif'
 
-	if Options.resize is not None:
-		resize = Options.resize.split('x')
-		if len(resize) < 2:
-			resize.append('-1')
-		args.extend(['-vf','scale=%s:%s' % (resize[0], resize[1])])
-		Output += '.r%s' % Options.resize
+    if Options.resize is not None:
+        resize = Options.resize.split('x')
+        if len(resize) < 2:
+            resize.append('-1')
+        args.extend(['-vf','scale=%s:%s' % (resize[0], resize[1])])
+        Output += '.r%s' % Options.resize
 
-	Output += '.' + Options.type
+    Output += '.' + Options.type
 
-	if not os.path.isdir(Output):
-		os.makedirs(Output)
-	if not os.path.isdir(Output):
-		print('ERROR: Can`t create output folder: ' + Output)
-		sys.exit(1)
+    if not os.path.isdir(Output):
+        os.makedirs(Output)
+    if not os.path.isdir(Output):
+        print('ERROR: Can`t create output folder: ' + Output)
+        sys.exit(1)
 
-	imgname = Options.imgname
-	if imgname is None:
-		imgname = os.path.basename( Input)
-		imgname,ext = os.path.splitext( imgname)
-	Output = os.path.join(Output, imgname + '.%0' + str(Options.padding) + 'd.' + Options.type)
+    imgname = Options.imgname
+    if imgname is None:
+        imgname = os.path.basename( Input)
+        imgname,ext = os.path.splitext( imgname)
+    Output = os.path.join(Output, imgname + '.%0' + str(Options.padding) + 'd.' + Options.type)
 
-	args.append(Output)
+    args.append(Output)
 
 else:
-	args = []
-	if Codec.find('.') == -1:
-		Codec += '.ffmpeg'
-	if os.path.dirname(Codec) == '':
-		Codec = os.path.join(CODECSDIR, Codec)
-	if not os.path.isfile(Codec):
-		print('ERROR: Can`t find codec "%s"' % Codec)
-		sys.exit(1)
+    args = []
+    if Codec.find('.') == -1:
+        Codec += '.ffmpeg'
+    if os.path.dirname(Codec) == '':
+        Codec = os.path.join(CODECSDIR, Codec)
+    if not os.path.isfile(Codec):
+        print('ERROR: Can`t find codec "%s"' % Codec)
+        sys.exit(1)
 
-	with open(Codec) as f:
-		lines = f.readlines()
+    with open(Codec) as f:
+        lines = f.readlines()
 
-	cmd_enc = lines[len(lines) - 1].strip()
-	if len(cmd_enc) < 2:
-		print('Invalid encode file "%s"' % Codec)
-		sys.exit(1)
+    cmd_enc = lines[len(lines) - 1].strip()
+    if len(cmd_enc) < 2:
+        print('Invalid encode file "%s"' % Codec)
+        sys.exit(1)
 
-	Output += '.' + os.path.basename(Codec.split('.')[0])
+    Output += '.' + os.path.basename(Codec.split('.')[0])
 
-	auxargs = []
-	if Options.resize is not None or Options.watermark is not None:
-		filter_complex = ''
-		if Options.resize is not None:
-			resize = Options.resize.split('x')
-			if len(resize) < 2:
-				hresize.append('-1')
-			filter_complex += 'scale=%s:-1,crop=%s:min(%s\\,ih),pad=%s:%s:0:max((%s-ih)/2\\,0)' % (resize[0], resize[0],resize[1], resize[0],resize[1],resize[1])
-			Output += '.r%s' % Options.resize
-		if Options.watermark is not None:
-			if not os.path.isfile( Options.watermark):
-				print('ERROR: Watermark file does not exist:\n' + Options.watermark)
-				sys.exit(1)
-			auxargs.extend(['-i',Options.watermark])
-			if len(filter_complex): filter_complex += ','
-			filter_complex += 'overlay=(main_w-overlay_w)/2:(main_h-overlay_h)/2'
-		auxargs.extend(['-filter_complex', filter_complex])
-	if Options.timestart != '':
-		auxargs.extend(['-ss', Options.timestart])
-	if Options.duration != '':
-		auxargs.extend(['-t', Options.duration])
+    auxargs = []
+    if Options.resize is not None or Options.watermark is not None:
+        filter_complex = ''
+        if Options.resize is not None:
+            resize = Options.resize.split('x')
+            if len(resize) < 2:
+                hresize.append('-1')
+            filter_complex += 'scale=%s:-1,crop=%s:min(%s\\,ih),pad=%s:%s:0:max((%s-ih)/2\\,0)' % (resize[0], resize[0],resize[1], resize[0],resize[1],resize[1])
+            Output += '.r%s' % Options.resize
+        if Options.watermark is not None:
+            if not os.path.isfile( Options.watermark):
+                print('ERROR: Watermark file does not exist:\n' + Options.watermark)
+                sys.exit(1)
+            auxargs.extend(['-i',Options.watermark])
+            if len(filter_complex): filter_complex += ','
+            filter_complex += 'overlay=(main_w-overlay_w)/2:(main_h-overlay_h)/2'
+        auxargs.extend(['-filter_complex', filter_complex])
+    if Options.timestart != '':
+        auxargs.extend(['-ss', Options.timestart])
+    if Options.duration != '':
+        auxargs.extend(['-t', Options.duration])
 
-	cmd_enc = cmd_enc.split(' ')
-	for arg_enc in cmd_enc:
-		if arg_enc[0] == '"':
-			arg_enc = arg_enc[1:]
-		if arg_enc[-1] == '"':
-			arg_enc = arg_enc[:-1]
+    cmd_enc = cmd_enc.split(' ')
+    for arg_enc in cmd_enc:
+        if arg_enc[0] == '"':
+            arg_enc = arg_enc[1:]
+        if arg_enc[-1] == '"':
+            arg_enc = arg_enc[:-1]
 
-		arg_enc = arg_enc.replace('@MOVIEMAKER@', MOVIEMAKER)
-		arg_enc = arg_enc.replace('@CODECSDIR@', CODECSDIR)
-		arg_enc = arg_enc.replace('@FPS@', Options.fps)
-		arg_enc = arg_enc.replace('@CONTAINER@', Options.container)
-		arg_enc = arg_enc.replace('@OUTPUT@', Output)
+        arg_enc = arg_enc.replace('@MOVIEMAKER@', MOVIEMAKER)
+        arg_enc = arg_enc.replace('@CODECSDIR@', CODECSDIR)
+        arg_enc = arg_enc.replace('@FPS@', Options.fps)
+        arg_enc = arg_enc.replace('@CONTAINER@', Options.container)
+        arg_enc = arg_enc.replace('@OUTPUT@', Output)
 
-		if arg_enc == '@AVCMD@':
-			args.append(Options.avcmd)
-			if StartNumber:
-				args.extend(['-start_number', str(StartNumber)])
-		elif arg_enc == '@INPUT@':
-			args.append( Input)
-		elif arg_enc == '@AUXARGS@':
-			args.extend(auxargs)
-		elif len(arg_enc):
-			args.append(arg_enc)
+        if arg_enc == '@AVCMD@':
+            args.append(Options.avcmd)
+            if StartNumber:
+                args.extend(['-start_number', str(StartNumber)])
+        elif arg_enc == '@INPUT@':
+            args.append( Input)
+        elif arg_enc == '@AUXARGS@':
+            args.extend(auxargs)
+        elif len(arg_enc):
+            args.append(arg_enc)
 
 print(' '.join(args))
 
 try:
-	process = subprocess.Popen(args, shell=False, stderr=subprocess.PIPE)
+    process = subprocess.Popen(args, shell=False, stderr=subprocess.PIPE)
 except Exception as e:
-	print('Command execution error:')
-	print(str(e))
-	sys.exit(1)
+    print('Command execution error:')
+    print(str(e))
+    sys.exit(1)
 
 re_duration = re.compile(r'Duration: (\d\d:\d\d:\d\d)\.(\d\d)')
 re_fps = re.compile(r'Stream.*: Video:.*(\d\d) fps')
@@ -207,91 +207,107 @@ img_old      = None
 time_img     = 0
 
 while True:
-	data = process.stderr.read(1)
-	if data is None:
-		break
-	#print( str( type( data)))
-	if len(data) < 1:
-		break
-	if not isinstance(data, str):
-		try:
-			data = str(data, 'ascii')
-		except:
-			data = '@'
-	data = data.replace('\r', '\n')
-	sys.stdout.write(data)
-	if data[-1] == '\n':
-		output = ''
-		if frame_old != frame:
-			frame_info = 'Frame = %d' % frame
-			if frames_total != -1:
-				frame_info += ' of %d' % frames_total
-			print(frame_info)
-			if progress != -1:
-				print('PROGRESS: %d%%' % progress)
+    data = process.stderr.read(1)
+    if data is None:
+        break
+    #print( str( type( data)))
+    if len(data) < 1:
+        break
+    if not isinstance(data, str):
+        try:
+            data = str(data, 'ascii')
+        except:
+            data = '@'
 
-				# Output thumbnail on-the-fly but not often than 10 % and 5 seconds:
-				if progress >= progress_old and time.time() > time_img + 5:
-					if len(Sequence):
-						img = int( .01 * len(Sequence) * progress)
-						if img >= len(Sequence): img = len(Sequence) - 1
-						if img != img_old:
-							print('@IMAGE!@' + Sequence[img])
-							img_old = img
-					elif frame > -1:
-						if Codec is None:
-							print('@IMAGE!@' + Output % frame)
-					progress_old += 5
-					time_img = time.time()
 
-			frame_old = frame
-		sys.stdout.flush()
-		continue
-	output += data
+    data = data.replace('\r', '\n')
+    sys.stdout.write(data)
 
-	if seconds == -1 and frame == -1:
-		reobj = re_duration.search(output)
-		if reobj is not None:
-			time_s, time_f = reobj.groups()
-			time_s = time_s.split(':')
-			time_slen = len(time_s)
-			if time_slen > 0:
-				seconds = 0
-				i = time_slen - 1
-				mult = 1
-				while i >= 0:
-					seconds += int(time_s[i]) * mult
-					mult *= 60
-					i -= 1
-				seconds = seconds * 100 + int(time_f)
-			output = ''
-			continue
 
-	if fps == -1 and frame == -1:
-		reobj = re_fps.search(output)
-		if reobj is not None:
-			fps = int(reobj.groups()[0])
-			output = ''
-			continue
+    if data[-1] == '\n':
 
-	if frame == -1 and fps != -1 and seconds != -1 and frames_total == -1:
-		frames_total = 1 + seconds * fps / 100
-		progress = 0
+        if output.find('error') != -1 or output.find('Error') != -1 or output.find('ERROR') != -1:
+            process.kill()
+            print('[ PARSER BAD RESULT ]')
+            break
 
-	if output == 'frame=':
-		framereached = True
-		output = ''
-		continue
-	if framereached and output[-4:] == 'fps=':
-		try:
-			frame = int(output[:-4])
-			if progress != -1 and frames_total > 0:
-				progress = int(100 * frame / frames_total)
-		except Exception as e:
-			print(str(e))
-		framereached = False
-		output = ''
-	continue
+        if frame_old != frame:
+            frame_info = 'Frame = %d' % frame
+            if frames_total != -1:
+                frame_info += ' of %d' % frames_total
+            print(frame_info)
+            if progress != -1:
+                print('PROGRESS: %d%%' % progress)
+
+                # Output thumbnail on-the-fly but not often than 10 % and 5 seconds:
+                if progress >= progress_old and time.time() > time_img + 5:
+                    if len(Sequence):
+                        img = int( .01 * len(Sequence) * progress)
+                        if img >= len(Sequence): img = len(Sequence) - 1
+                        if img != img_old:
+                            print('@IMAGE!@' + Sequence[img])
+                            img_old = img
+                    elif frame > -1:
+                        if Codec is None:
+                            print('@IMAGE!@' + Output % frame)
+                    progress_old += 5
+                    time_img = time.time()
+
+            frame_old = frame
+        sys.stdout.flush()
+
+        output = ''
+
+        continue
+
+
+    output += data
+
+
+    if seconds == -1 and frame == -1:
+        reobj = re_duration.search(output)
+        if reobj is not None:
+            time_s, time_f = reobj.groups()
+            time_s = time_s.split(':')
+            time_slen = len(time_s)
+            if time_slen > 0:
+                seconds = 0
+                i = time_slen - 1
+                mult = 1
+                while i >= 0:
+                    seconds += int(time_s[i]) * mult
+                    mult *= 60
+                    i -= 1
+                seconds = seconds * 100 + int(time_f)
+            output = ''
+            continue
+
+    if fps == -1 and frame == -1:
+        reobj = re_fps.search(output)
+        if reobj is not None:
+            fps = int(reobj.groups()[0])
+            output = ''
+            continue
+
+    if frame == -1 and fps != -1 and seconds != -1 and frames_total == -1:
+        frames_total = 1 + seconds * fps / 100
+        progress = 0
+
+    if output == 'frame=':
+        framereached = True
+        output = ''
+        continue
+
+    if framereached and output[-4:] == 'fps=':
+        try:
+            frame = int(output[:-4])
+            if progress != -1 and frames_total > 0:
+                progress = int(100 * frame / frames_total)
+        except Exception as e:
+            print(str(e))
+        framereached = False
+        output = ''
+        continue
 
 
 returncode = process.wait()
