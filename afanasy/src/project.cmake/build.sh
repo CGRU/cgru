c258089d0269018578c65a9207148c4c1788da24
timurhai <timurhai@gmail.com>
2017-01-07 07:17:16 -0500

    afanasy qt gui: debian9 moved on qt5

--- afanasy/src/project.cmake/build.sh
+++ afanasy/src/project.cmake/build.sh
@@ -47,6 +47,10 @@ case ${DISTRIBUTIVE} in
     Debian)
         export ADD_CMAKE_MODULE_PATH="$PWD"
         export AF_ADD_LFLAGS="$AF_ADD_LFLAGS -lpthread -lrt"
+		export AF_QT_VER="5"
+		if [[ "$DISTRIBUTIVE_VERSION" < "9" ]]; then
+			export AF_QT_VER="4"
+		fi
         ;;
     Gentoo)
         ;;
