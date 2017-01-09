96a8bf11381f53e9565d039d8d13eeedb1f16a93
timurhai <timurhai@gmail.com>
2017-01-09 15:53:34 +0300

    windows build tries to find latest Qt5.7* and boost*

--- afanasy/src/project.cmake/win_build_msvc.cmd
+++ afanasy/src/project.cmake/win_build_msvc.cmd
@@ -5,20 +5,36 @@ pushd ..\..\..
 call setup.cmd
 popd
 
+setlocal EnableDelayedExpansion
+
 rem Cmake finds 'Qt' by searching for 'qmake' in 'PATH'
-set "cgru_qt=C:\Qt\Qt5.7.0\5.7\msvc2015_64"
-if exist %cgru_qt% SET "PATH=%cgru_qt%\bin;%PATH%"
+SET "cgru_qt_install_dir=C:\Qt"
+For /F "Tokens=*" %%I in ('dir /b "%cgru_qt_install_dir%\Qt5.7*"') Do SET "cgru_qt=%%I"
+if defined cgru_qt (
+	SET "cgru_qt=!cgru_qt_install_dir!\!cgru_qt!\5.7\msvc2015_64"
+	echo Adding "!cgru_qt!" to PATH
+	SET "PATH=!cgru_qt!\bin;%PATH%"
+)
 set "AF_GUI=YES"
 set "AF_QT_VER=5"
 
 rem Specify Python:
 if exist %CGRU_LOCATION%\python-devel set CGRU_PYTHONDIR=%CGRU_LOCATION%\python-devel
 if defined CGRU_PYTHONDIR (
-   echo Building with CGRU Python: %CGRU_PYTHONDIR%
-   SET AF_PYTHON_INCLUDE_PATH=%CGRU_PYTHONDIR%\include
-   SET AF_PYTHON_LIBRARIES=%CGRU_PYTHONDIR%\libs\python34.lib
+   echo Building with CGRU Python: "!CGRU_PYTHONDIR!"
+   SET "AF_PYTHON_INCLUDE_PATH=!CGRU_PYTHONDIR!\include"
+   SET "AF_PYTHON_LIBRARIES=!CGRU_PYTHONDIR!\libs\python34.lib"
 )
 
+rem Boost:
+set "cgru_boost_install_dir=C:\local"
+For /F "Tokens=*" %%I in ('dir /b "%cgru_boost_install_dir%\boost*"') Do set "BOOST_ROOT=%%I"
+if defined BOOST_ROOT (
+	set "BOOST_ROOT=!cgru_boost_install_dir!\!BOOST_ROOT!"
+	echo BOOST_ROOT set to "!BOOST_ROOT!"
+)
+
+rem Local overrides:
 if exist override.cmd call override.cmd
 
 rem Get Afanasy sources revision:
