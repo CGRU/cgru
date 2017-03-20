38738cc40d722a8a2774ba90fa88dc77079488c9
Erkan Ozgur Yilmaz <eoyilmaz@gmail.com>
2017-03-20 20:21:38 +0300

    Fix: Fixed Maya Redshift progress messages to be reported properly for multiframe renders. Fix: Fixed XSI Redshift progress messages to be reported properly for multiframe renders.

--- afanasy/python/parsers/xsi_redshift.py
+++ afanasy/python/parsers/xsi_redshift.py
@@ -3,61 +3,58 @@
 from parsers import parser
 
 import re
+
 # INFO : [Redshift] 	Block 32/48 (7,4) rendered by GPU 0 in 2ms
-re_percent = re.compile(r'(Block*)(\s*)(\d*)(\/)(\d*)(\s*)(\S*)(\s*)(rendered by GPU)')
-re_frame = re.compile(r'Rendering frame [0-9]+')
+re_percent = re.compile(
+    r'(Block*)(\s*)(\d*)(\/)(\d*)(\s*)(\S*)(\s*)(rendered by GPU)')
+re_frame = re.compile(r'Rendering.*frame [0-9]+')
+
 
 class xsi_redshift(parser.parser):
-	"""Softimage Redshift
-	"""
+    """Softimage Redshift
+    """
 
-	def __init__(self):
-		parser.parser.__init__(self)
-		self.firstframe = True
-		self.data_all = ''
+    def __init__(self):
+        parser.parser.__init__(self)
+        self.firstframe = True
+        self.data_all = ''
 
-	def do(self, data, mode):
-		"""Missing DocString
+    def do(self, data, mode):
+        """Missing DocString
 
-		:param data:
-		:param mode:
-		:return:
-		"""
+        :param data:
+        :param mode:
+        :return:
+        """
 
-		if len(data) < 1:
-			return
-		
-		frame = False
-		block = 0
-		blockCount = 0
-		
-		match = re_frame.search(data)
-		if match is not None:
-			frame = True
+        if len(data) < 1:
+            return
 
+        frame = False
+        block = 0
+        blockCount = 0
 
-		#print("cur frame " + str(self.frame))
+        match = re_frame.search(data)
+        if match is not None:
+            frame = True
 
+        # progress line?
+        match = re_percent.findall(data)
+        if len(match):
+            # get current block
+            block = float(match[0][2])
+            # get blockCount
+            blockCount = float(match[0][4])
 
-		# progress line?
-		match = re_percent.findall(data)
-		if len(match):
-			# get current block
-			block = float(match[0][2])
-			# get blockCount
-			blockCount = float(match[0][4])
+            # calculate percentage
+            percentframe = float(100 / (blockCount / block))
+            self.percent = int(percentframe)
+            self.percentframe = int(percentframe)
 
-			# calculate percentage
-			percentframe = float(100/(blockCount/block))
-			self.percent = int(percentframe)
-			self.percentframe = int(percentframe)
+        self.calculate()
 
-		self.calculate()
+        if frame:
+            if not self.firstframe:
+                self.frame += 1
 
-		if frame:
-			if not self.firstframe:
-				self.frame += 1
-
-			self.firstframe = False
-
-
+            self.firstframe = False
