2850f2c5843adeda7689d0b3ed4a93267fd16b7c
Erkan Ozgur Yilmaz <eoyilmaz@gmail.com>
2017-04-10 19:26:11 +0300

    Fix: Fixed XSI Redshift now it properly reports multi frame progress messages.

--- afanasy/python/parsers/xsi_redshift.py
+++ afanasy/python/parsers/xsi_redshift.py
@@ -4,9 +4,11 @@ from parsers import parser
 
 import re
 
-# INFO : [Redshift] 	Block 32/48 (7,4) rendered by GPU 0 in 2ms
+# INFO : [Redshift] Block 32/48 (7,4) rendered by GPU 0 in 2ms
+#        [Redshift] Block 126/135 (14,0) rendered by GPU 1 in 12ms
 re_percent = re.compile(
-    r'(Block*)(\s*)(\d*)(\/)(\d*)(\s*)(\S*)(\s*)(rendered by GPU)')
+    r'(Block*)(\s*)(\d*)(\/)(\d*)(\s*)(\S*)(\s*)(rendered by GPU.*)'
+)
 re_frame = re.compile(r'Rendering.*frame [0-9]+')
 
 
@@ -16,8 +18,13 @@ class xsi_redshift(parser.parser):
 
     def __init__(self):
         parser.parser.__init__(self)
-        self.firstframe = True
         self.data_all = ''
+        self.first_frame = True
+
+        self.str_error = ['Frame rendering aborted']
+
+        self.block = 0
+        self.block_count = 0
 
     def do(self, data, mode):
         """Missing DocString
@@ -30,31 +37,28 @@ class xsi_redshift(parser.parser):
         if len(data) < 1:
             return
 
-        frame = False
-        block = 0
-        blockCount = 0
-
         match = re_frame.search(data)
         if match is not None:
-            frame = True
+            if not self.first_frame:
+                self.frame += 1
+            else:
+                self.first_frame = False
+            self.block = 0
+            self.block_count = 0
+            self.percentframe = 0
 
-        # progress line?
         match = re_percent.findall(data)
-        if len(match):
+        if match:
             # get current block
-            block = float(match[0][2])
-            # get blockCount
-            blockCount = float(match[0][4])
+            matched_block = float(match[0][2])
+            self.block = max(self.block, matched_block)
+
+            # get block_count
+            found_block_count = float(match[0][4])
+            self.block_count = max(self.block_count, found_block_count)
 
             # calculate percentage
-            percentframe = float(100 / (blockCount / block))
-            # self.percent = int(percentframe)
-            self.percentframe = int(percentframe)
+            self.percentframe = \
+                int(100.0 * self.block / float(self.block_count))
 
         self.calculate()
-
-        if frame:
-            if not self.firstframe:
-                self.frame += 1
-
-            self.firstframe = False
