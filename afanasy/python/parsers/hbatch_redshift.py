5ee344d10c21a22edd2667d87b7060a8fb062e2b
elecstorm <elecstorm@yandex.ru>
2017-11-08 14:26:02 +0300

    Update hbatch_redshift.py
    Changed to hbatch_mantra-like parser.
    Redsfhit parser moved to redshift.py.
    Added saving errors from hbatch parser.

--- afanasy/python/parsers/hbatch_redshift.py
+++ afanasy/python/parsers/hbatch_redshift.py
@@ -1,24 +1,27 @@
 # -*- coding: utf-8 -*-
 
 from parsers import hbatch
+from parsers import redshift
 
 
-class hbatch_redshift(hbatch.hbatch):
+class hbatch_redshift(hbatch.hbatch, redshift.redshift):
+    """Houdini batch + catch redshift node output
+    """
 
     def __init__(self):
         hbatch.hbatch.__init__(self)
 
-        self.str_error += [
-            '[Redshift]No devices found. Aborting rendering.',
-            'Frame aborted',
-            'Bad node type found: Redshift_ROP',
-            'Bad node type found: redshift_vopnet']
+        #Saving the error list from hbatch
+        temp_str_error = self.str_error
+        temp_str_warning = self.str_warning
+        temp_str_badresult = self.str_badresult
 
-        self.str_warning += []
+        redshift.redshift.__init__(self)
 
-        self.str_badresult += [
-            'Frame rendering aborted']
-            
+        #Restore the list of errors of batch
+        self.str_error += temp_str_error
+        self.str_warning += temp_str_warning
+        self.str_badresult += temp_str_badresult
 
     def do(self, data, mode):
         """Missing DocString
@@ -27,5 +30,6 @@ class hbatch_redshift(hbatch.hbatch):
         :param mode:
         :return:
         """
-
+        # print data
+        redshift.redshift.do(self, data, mode)
         hbatch.hbatch.do(self, data, mode)
