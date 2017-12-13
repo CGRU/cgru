2dc535de9d1b51ae620fda60bccbb0badc3fde41
Alexandre Buisine <alexandrejabuisine@gmail.com>
2017-12-13 13:07:19 +0100

    add a ffmpeg frame based progress parser
    just echo the numbers of frames like this "Frames:<frame_count>" at the beginning the command
    will fall back to automatic "Duration:" based if not available

--- afanasy/python/parsers/ffmpeg.py
+++ afanasy/python/parsers/ffmpeg.py
@@ -4,10 +4,12 @@ from parsers import parser
 
 import re
 
-re_time		= re.compile(r'(\d{2}):(\d{2}):(\d{2}).(\d{2})')
-re_position	= re.compile(r'time=(\d{2}:\d{2}:\d{2}.\d{2})')
-re_duration	= re.compile(r'Duration: (\d{2}:\d{2}:\d{2}.\d{2})')
+re_time				= re.compile(r'(\d{2}):(\d{2}):(\d{2}).(\d{2})')
 
+re_duration			= re.compile(r'Duration: *(\d{2}:\d{2}:\d{2}.\d{2})')
+re_frames			= re.compile(r'Frames: *(\d+)')
+re_position_time	= re.compile(r'time=(\d{2}:\d{2}:\d{2}.\d{2})')
+re_position_frame	= re.compile(r'frame= *(\d+) ')
 
 class ffmpeg(parser.parser):
 	"""ffmpeg command parser
@@ -18,6 +20,8 @@ class ffmpeg(parser.parser):
 		self.str_badresult	= ['Output file is empty']
 		self.str_error		= ['Invalid']
 		self.duration		= 0.1
+		self.frame			= 0
+		self.frames			= 0
 
 	def parseTime(self, time):
 		res = re_time.match(time)
@@ -35,13 +39,29 @@ class ffmpeg(parser.parser):
 		:return:
 		"""
 
+		res = re_frames.findall(data)
+		if len(res):
+			self.frames = int(res[0])
+
 		res = re_duration.findall(data)
 		if len(res):
 			self.duration = self.parseTime(res[0])
 
-		res = re_position.findall(data)
-		if len(res):
-			time = self.parseTime(res[-1])
-			self.percentframe = int(time / self.duration * 100)
+		position_frame	= 0
+		position_time	= 0
+		if ( self.frames ):
+			res = re_position_frame.findall(data)
+			if len(res):
+				position_frame = int(res[0])
+		else:
+			res = re_position_time.findall(data)
+			if len(res):
+				position_time = self.parseTime(res[-1])
 
+		if ( position_frame ):
+			self.percentframe = int(position_frame / float(self.frames) * 100)
+			self.calculate()
+
+		if ( position_time ):
+			self.percentframe = int(position_time / float(self.duration) * 100)
 			self.calculate()
