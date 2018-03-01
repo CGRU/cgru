1c1e8d452b6600f28c065bae83775864fe2913ca
timurhai <timurhai@gmail.com>
2018-03-01 15:05:43 +0300

    openexr build scripts update

--- utilities/openexr/build_openexr.sh
+++ utilities/openexr/build_openexr.sh
@@ -9,7 +9,7 @@ LDFLAGS="-L/lib64 -L/usr/lib64 -L$prefix/lib -L$prefix/lib64 -lpthread"
 export CPPFLAGS
 export LDFLAGS
 
-cd openexr*
+cd openexr*/
 
 if [ ! -z $1 ] ; then
    ./configure -h; exit
