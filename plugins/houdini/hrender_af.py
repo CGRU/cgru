e4a1bb1c58d5d43fcb13deff2828179e1686cb6e
Nikita Musatov <45572972+notawhalevfx@users.noreply.github.com>
2018-12-21 13:11:44 +0300

    Alembic fix error if can't change parm

--- plugins/houdini/hrender_af.py
+++ plugins/houdini/hrender_af.py
@@ -204,8 +204,9 @@ elif drivertypename == "arnold":
                 print('Failed, frame progress not available.')
 
 elif drivertypename == "alembic":
-    ropnode.parm('lpostframe').set('python')
-    expr = r'''import sys
+    try:
+        ropnode.parm('lpostframe').set('python')
+        expr = r'''import sys
 f = hou.parmTuple('f').eval()
 percent = int(100*(hou.frame()-f[0])/(f[1]-f[0]))
 
@@ -213,7 +214,9 @@ out = 'ABC_PROGRESS ' + str(percent) + '%\n'
 
 sys.stdout.write(out)
 sys.stdout.flush()'''
-    ropnode.parm('postframe').set(expr)
+        ropnode.parm('postframe').set(expr)
+    except:
+        print('Failed, frame progress not available.')
 
 elif drivertypename == "Redshift_ROP":
     # Trying to set ROP to output progress
