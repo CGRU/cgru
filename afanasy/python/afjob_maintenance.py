377c7e4ebb14a7cf7953fb5af989a6708fcb94b9
timurhai <timurhai@gmail.com>
2021-01-22 13:48:29 +0300

    afanasy: afjob_maintenance.py '--mask' argument to run on machines by mask.
    It uses afcmd.getRenderList() to get renders names by mask.

--- afanasy/python/afjob_maintenance.py
+++ afanasy/python/afjob_maintenance.py
@@ -1,30 +1,47 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 import af
+import afcmd
 
 from optparse import OptionParser
 
 Parser = OptionParser(
-    usage="%prog render1 render2 ... command\ntype \"%prog -h\" for "
+    usage="\
+    \n%prog render1 render2 ... command\
+    \n%prog --mask 'regexp' command\
+    \ntype \"%prog -h\" for "
     "help", version="%prog 1.  0"
 )
-Parser.add_option('-n', '--jobname',     dest='jobname',     type='string',       default=None,      help='Job name')
-Parser.add_option('-s', '--service',     dest='service',     type='string',       default='generic', help='Service type')
-Parser.add_option('-p', '--parser',      dest='parser',      type='string',       default='generic', help='Parser type')
-Parser.add_option('-m', '--maxruntasks', dest='maxruntasks', type='int',          default=5,         help='Maximum running tasks')
-Parser.add_option('-o', '--offline',     dest='offline',     action='store_true', default=False,     help='Send job offline (paused)')
-Parser.add_option('-i', '--ignore',      dest='ignore',      action='store_true', default=False,     help='Ignore render Nimby and Paused states')
+Parser.add_option(      '--jobname',     dest='jobname',     type='string',       default=None,      help='Job name')
+Parser.add_option(      '--service',     dest='service',     type='string',       default='generic', help='Service type')
+Parser.add_option(      '--parser',      dest='parser',      type='string',       default='generic', help='Parser type')
+Parser.add_option(      '--maxruntasks', dest='maxruntasks', type='int',          default=5,         help='Maximum running tasks')
+Parser.add_option(      '--mask',        dest='hostsmask',   type='string',       default=None,      help='Hosts mask')
+Parser.add_option(      '--offline',     dest='offline',     action='store_true', default=False,     help='Send job offline (paused)')
+Parser.add_option(      '--ignore',      dest='ignore',      action='store_true', default=False,     help='Ignore render Nimby and Paused states')
 Parser.add_option('-V', '--verbose',     dest='verbose',     action='store_true', default=False,     help='Verbose mode')
 Parser.add_option('-D', '--debug',       dest='debug',       action='store_true', default=False,     help='Debug mode (verbose mode, no commands execution)')
 (Options, Args) = Parser.parse_args()
 
-if len(Args) < 2:
-    Parser.error('Not enough arguments provided.')
+if len(Args) < 1:
+    Parser.error('You should specify command at least.')
 
 if Options.debug:
     Options.verbose = True
 
-Renders = Args[:-1]
+Renders = []
+if Options.hostsmask:
+    for render in afcmd.getRenderList(Options.hostsmask):
+        Renders.append(render.name)
+else:
+    if len(Args) < 2:
+        Parser.error('You should specify renders to run on.')
+    Renders = Args[:-1]
+
+if len(Renders) == 0:
+    Parser.error("No renders found.")
+
 Command = Args[-1]
 JobName = Options.jobname
 if JobName is None:
