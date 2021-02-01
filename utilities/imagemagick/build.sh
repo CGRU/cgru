da85c6d04533370262d482a60cd1e9af1da45cff
timurhai <timurhai@gmail.com>
2021-02-01 09:40:37 +0000

    imagemagick build updates

--- utilities/imagemagick/build.sh
+++ utilities/imagemagick/build.sh
@@ -6,23 +6,12 @@ exr=`dirname $PWD`/openexr
 
 prefix=$PWD
 
-export OPENEXR_CFLAGS="-I$exr/include/OpenEXR"
-export OPENEXR_LIBS="-L$exr/lib -L$exr/lib64 -lIlmImf -lz -lImath -lHalf -lIex -lIlmThread -lpthread"
-export LDFLAGS="-B/usr/lib/gold-ld/ -lstdc++"
+if [ -d "$exr/include" ]; then
+	export OPENEXR_CFLAGS="-I$exr/include/OpenEXR"
+	export OPENEXR_LIBS="-L$exr/lib -L$exr/lib64 -lIlmImf -lz -lImath -lHalf -lIex -lIlmThread -lpthread"
+fi
 
 cd $im
-
-configure="configure"
-configure_cgru="$configure.cgru"
-if [ ! -f "$configure_cgru" ] ; then
-   echo "Processing $configure"
-   mv -vf $configure $configure_cgru
-   sed \
-   -e "s:OPENEXR_CFLAGS=\"\":#OPENEXR_CFLAGS=\"\":g" \
-   -e "s:OPENEXR_LIBS=\"\":#OPENEXR_LIBS=\"\":g" \
-   < "${configure_cgru}" > "${configure}"
-   chmod a+rwx $configure
-fi
 
 configure="./configure"
 configure="$configure --prefix=$prefix"
@@ -33,9 +22,9 @@ configure="$configure --enable-shared="
 #with-quantum-depth=32
 
 if [ -z "$1" ]; then
-   #make clean
+   make clean
    $configure
-   make $@
+   make -j2 $@
    convert='utilities/convert'
    [ -f $convert ] && mv -vf $convert ../../../bin
 else
