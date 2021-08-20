3dbece141c140d5fe7c95ceeba1b29aad0f353cb
mkonnov <maxim.konnov@parovoz.tv>
2021-08-20 12:20:19 +0300

    Shotgun and Ftrack Services

---
+++ afanasy/python/services/shotgun.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from services import service
+
+parser = 'shotgun'
+
+
+class shotgun(service.service):
+    """Shotgun service for Publish, Send Review, etc.
+    """
+    pass
