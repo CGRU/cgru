3e81ce6bc7bde3609441292f57a4df34084e2b01
timurhai <timurhai@gmail.com>
2022-04-01 10:51:08 +0300

    build with no fermer gui by default

--- afanasy/src/project.cmake/build.sh
+++ afanasy/src/project.cmake/build.sh
@@ -20,11 +20,11 @@ popd > /dev/null
 options=""
 sql="REQUIRED"
 gui="YES"
-fermer="YES"
+fermer="NO"
 for arg in "$@"; do
 	[ $arg == "--nosql" ] && sql="NO" && shift
 	[ $arg == "--nogui" ] && gui="NO" && shift
-	[ $arg == "--nofermer" ] && fermer="NO" && shift
+	[ $arg == "--fermer" ] && fermer="YES" && shift
 	[ $arg == "--debug" ] && debug="-g" && shift
 done
 
--- afanasy/src/render/res_gpu_nvidia.cpp
+++ afanasy/src/render/res_gpu_nvidia.cpp
@@ -2,4 +2,5 @@
 
 bool GetGPUInfo_NVIDIA(af::HostRes & o_hres, bool i_verbose)
 {
+	return true;
 }
