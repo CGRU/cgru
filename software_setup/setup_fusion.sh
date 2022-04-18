60c51da794788216d90c647c5a21778848033983
Erkan Ozgur Yilmaz <eoyilmaz@gmail.com>
2022-04-18 15:35:33 +0100

    * [Fusion] Do not override ``FUSION_LOCATION`` variable if it is already defined before calling ``setup_fusion.sh``.

--- software_setup/setup_fusion.sh
+++ software_setup/setup_fusion.sh
@@ -14,23 +14,24 @@ else
     FUSION_INSTALL_DIR="/opt/BlackmagicDesign"
 fi
 
-FUSION_FOLDERS=`ls "$FUSION_INSTALL_DIR"`
-FUSION_LOCATION=""
-FUSION_EXEC=""
-for FUSION_FOLDER in $FUSION_FOLDERS ;
-do
-   if [ "`echo $FUSION_FOLDER | awk '{print match( \$1, "Fusion")}'`" == "1" ]; then
-      FUSION_LOCATION="${FUSION_INSTALL_DIR}/${FUSION_FOLDER}"
-      if [ "`uname`" == "Darwin" ]; then
-        FUSION_EXEC="${FUSION_FOLDER}.app/${FUSION_FOLDER}"
-      else
-        FUSION_EXEC="Fusion"
-      fi
-   fi
-done
-
-export FUSION_LOCATION
-export FUSION_EXEC="${FUSION_LOCATION}/${FUSION_EXEC}"
+if [ -z "$FUSION_LOCATION" ]; then
+  FUSION_FOLDERS=`ls "$FUSION_INSTALL_DIR"`
+  FUSION_LOCATION=""
+  FUSION_EXEC=""
+  for FUSION_FOLDER in $FUSION_FOLDERS ;
+  do
+     if [ "`echo $FUSION_FOLDER | awk '{print match( \$1, "Fusion")}'`" == "1" ]; then
+        FUSION_LOCATION="${FUSION_INSTALL_DIR}/${FUSION_FOLDER}"
+        if [ "`uname`" == "Darwin" ]; then
+          FUSION_EXEC="${FUSION_FOLDER}.app/${FUSION_FOLDER}"
+        else
+          FUSION_EXEC="Fusion"
+        fi
+     fi
+  done
+  export FUSION_LOCATION
+  export FUSION_EXEC="${FUSION_LOCATION}/${FUSION_EXEC}"
+fi
 
 #
 # For export:
@@ -45,4 +46,3 @@ locate_fusion="$CGRU_LOCATION/software_setup/locate_fusion.sh"
 [ -f $locate_fusion ] && source $locate_fusion
 
 echo "FUSION = '${APP_EXE}'"
-
