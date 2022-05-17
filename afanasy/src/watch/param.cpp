5cbdef31610c7fe519338b22a4975b8f152efcb7
timurhai <timurhai@gmail.com>
2022-05-17 10:11:46 +0300

    afanasy: Qt deprecations

--- afanasy/src/watch/param.cpp
+++ afanasy/src/watch/param.cpp
@@ -173,10 +173,10 @@ bool Param::getInputDialog(const QVariant & i_var, QString & o_str, QWidget * i_
 			break;
 
 		o_str.clear();
-		QStringList list = value.split(",", Qt::SkipEmptyParts);
+		QStringList list = value.split(",", QString::SkipEmptyParts);
 		for (int i = 0; i < list.size(); i++)
 		{
-			QStringList pair = list[i].split(":", Qt::SkipEmptyParts);
+			QStringList pair = list[i].split(":", QString::SkipEmptyParts);
 			if (pair.size() != 2)
 			{
 				o_str = "Invalid pools string.";
