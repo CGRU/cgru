2f0b83384d8a7cced86b15aaedefb78096d51f7a
timurhai <timurhai@gmail.com>
2022-05-24 12:28:22 +0300

    keeper nimby: day name locale independent, tabs -> spaces

--- utilities/keeper/nimby.py
+++ utilities/keeper/nimby.py
@@ -11,109 +11,114 @@ free_set = False
 
 
 def setnimby():
-	cmd = af.Cmd().renderSetNimby()
-	render.refreshAfter()
+    cmd = af.Cmd().renderSetNimby()
+    render.refreshAfter()
 
 
 def setNIMBY():
-	cmd = af.Cmd().renderSetNIMBY()
-	render.refreshAfter()
+    cmd = af.Cmd().renderSetNIMBY()
+    render.refreshAfter()
 
 
 def setFree():
-	cmd = af.Cmd().renderSetFree()
-	render.refreshAfter()
+    cmd = af.Cmd().renderSetFree()
+    render.refreshAfter()
 
 def setFreeUnpause():
-	cmd = af.Cmd().renderSetFreeUnpause()
-	render.refreshAfter()
+    cmd = af.Cmd().renderSetFreeUnpause()
+    render.refreshAfter()
 
 
 def ejectTasks():
-	cmd = af.Cmd().renderEjectTasks()
-	render.refreshAfter()
+    cmd = af.Cmd().renderEjectTasks()
+    render.refreshAfter()
 
 def ejectAndNIMBY():
-	cmd = af.Cmd().renderEjectAndNIMBY()
-	render.refreshAfter()
+    cmd = af.Cmd().renderEjectAndNIMBY()
+    render.refreshAfter()
 
 
 def ejectNotMyTasks():
-	cmd = af.Cmd().renderEjectNotMyTasks()
-	render.refreshAfter()
+    cmd = af.Cmd().renderEjectNotMyTasks()
+    render.refreshAfter()
 
 
 def refresh(reset=False):
-	global nimby_set
-	global free_set
+    global nimby_set
+    global free_set
 
-	if reset:
-		nimby_set = False
-		free_set = False
+    days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun']
+    print('nimby.refresh')
 
-	toset_nimby = False
-	toset_free = False
-	toallow_tasks = False
-	toeject_tasks = False
+    if reset:
+        nimby_set = False
+        free_set = False
 
-	# Get current time and day:
-	daytime = datetime.now()
-	cur_time = daytime.strftime('%H:%M')
+    toset_nimby = False
+    toset_free = False
+    toallow_tasks = False
+    toeject_tasks = False
 
-	# Check yesterday free:
-	yesterday = (daytime - timedelta(days=1)).strftime('%a').lower()
-	var = 'nimby_' + yesterday
-	if var in cgruconfig.VARS:
-		values = cgruconfig.VARS[var].split(' ')
-		time_begin = values[0]
-		time_end = values[1]
-		if time_end < time_begin and 'Enable' in values:
-			if cur_time > time_end:
-				toset_free = True
-			else:
-				toset_nimby = True
-				if 'Eject' in values:
-					toeject_tasks = True
-				if 'nimby' in values:
-					toallow_tasks = True
-				if 'NIMBY' in values:
-					toallow_tasks = False
+    # Get current time and day:
+    daytime = datetime.now()
+    cur_time = daytime.strftime('%H:%M')
 
-	# Check today:
-	var = 'nimby_' + daytime.strftime('%a').lower()
-	if var in cgruconfig.VARS:
-		values = cgruconfig.VARS[var].split(' ')
-		time_begin = values[0]
-		time_end = values[1]
-		if time_begin != time_end and 'Enable' in values:
-			# Check free if it not tomorrow:
-			if cur_time > time_end > time_begin:
-				toset_free = True
-			# Check Nimby:
-			elif cur_time > time_begin:
-				toset_nimby = True
-				if 'Eject' in values:
-					toeject_tasks = True
-				if 'nimby' in values:
-					toallow_tasks = True
-				if 'NIMBY' in values:
-					toallow_tasks = False
+    # Check yesterday free:
+    yesterday = days[(daytime - timedelta(days=1)).weekday()]
+    var = 'nimby_' + yesterday
+    if var in cgruconfig.VARS:
+        values = cgruconfig.VARS[var].split(' ')
+        time_begin = values[0]
+        time_end = values[1]
+        if time_end < time_begin and 'Enable' in values:
+            if cur_time > time_end:
+                toset_free = True
+            else:
+                toset_nimby = True
+                if 'Eject' in values:
+                    toeject_tasks = True
+                if 'nimby' in values:
+                    toallow_tasks = True
+                if 'NIMBY' in values:
+                    toallow_tasks = False
 
-	# Set state:
-	if toset_nimby:
-		if not nimby_set:
-			if toallow_tasks:
-				setnimby()
-				if toeject_tasks:
-					ejectNotMyTasks()
-			else:
-				setNIMBY()
-				if toeject_tasks:
-					ejectTasks()
-			nimby_set = True
-			free_set = False
-	elif toset_free:
-		if not free_set:
-			setFree()
-			free_set = True
-			nimby_set = False
+    # Check today:
+    var = 'nimby_' + days[daytime.weekday()]
+    print(var)
+    if var in cgruconfig.VARS:
+        values = cgruconfig.VARS[var].split(' ')
+        time_begin = values[0]
+        time_end = values[1]
+        print(values, time_begin, time_end)
+        if time_begin != time_end and 'Enable' in values:
+            # Check free if it not tomorrow:
+            if cur_time > time_end > time_begin:
+                toset_free = True
+            # Check Nimby:
+            elif cur_time > time_begin:
+                toset_nimby = True
+                if 'Eject' in values:
+                    toeject_tasks = True
+                if 'nimby' in values:
+                    toallow_tasks = True
+                if 'NIMBY' in values:
+                    toallow_tasks = False
+
+    # Set state:
+    if toset_nimby:
+        if not nimby_set:
+            if toallow_tasks:
+                setnimby()
+                if toeject_tasks:
+                    ejectNotMyTasks()
+            else:
+                setNIMBY()
+                if toeject_tasks:
+                    ejectTasks()
+            nimby_set = True
+            free_set = False
+    elif toset_free:
+        if not free_set:
+            setFree()
+            free_set = True
+            nimby_set = False
