0cd45d92c997c2b9861b1ffad838b15d33bb11c2
timurhai <timurhai@gmail.com>
2022-06-15 09:53:00 +0300

    afanasy: passwd ancient style code formatted

--- afanasy/src/libafanasy/common/passwd.cpp
+++ afanasy/src/libafanasy/common/passwd.cpp
@@ -11,120 +11,120 @@ using namespace af;
 
 Passwd::Passwd()
 {
-   init();
+	init();
 }
 
-Passwd::Passwd( const std::string & VisorKey, const std::string & GodKey)
+Passwd::Passwd(const std::string & i_visor_key, const std::string & i_god_key)
 {
-   init();
-   std::string visor_str = VisorKey;
-   std::string   god_str = GodKey;
-   if( visor_str.size() > key_len) visor_str.resize(key_len);
-   if(   god_str.size() > key_len)   god_str.resize(key_len);
+	init();
+	std::string visor_str = i_visor_key;
+	std::string   god_str = i_god_key;
+	if (visor_str.size() > ms_key_len)
+		visor_str.resize(ms_key_len);
+	if (god_str.size() > ms_key_len)
+		god_str.resize(ms_key_len);
 
-   strncpy( (char*) visor, visor_str.c_str(), visor_str.size());
-   strncpy( (char*)   god,   god_str.c_str(),   god_str.size());
-//write(1,visor,key_len); write(1,"\n",1);
-//write(1,  god,key_len); write(1,"\n",1);
-   char ch1, ch2;
-   for( int d = 0; d < digest_len; d++)
-   {
-      ch1 = visor[d*2];
-      ch2 = visor[d*2+1];
-      ch1 = ((ch1 <= '9') ? (ch1 - '0') : (ch1 - 'a'+10));
-      ch2 = ((ch2 <= '9') ? (ch2 - '0') : (ch2 - 'a'+10));
-      visor[d] = 16*ch1 + ch2;
-      ch1 = god[d*2];
-      ch2 = god[d*2+1];
-      ch1 = ((ch1 <= '9') ? (ch1 - '0') : (ch1 - 'a'+10));
-      ch2 = ((ch2 <= '9') ? (ch2 - '0') : (ch2 - 'a'+10));
-      god[d] = 16*ch1 + ch2;
-   }
-//   Passwd::PrintKey( visor);
-//   Passwd::PrintKey( god );
+	strncpy((char*) m_visor, visor_str.c_str(), visor_str.size());
+	strncpy((char*)   m_god,   god_str.c_str(),   god_str.size());
+	char ch1, ch2;
+	for (int d = 0; d < ms_digest_len; d++)
+	{
+		ch1 = m_visor[d*2];
+		ch2 = m_visor[d*2+1];
+		ch1 = ((ch1 <= '9') ? (ch1 - '0') : (ch1 - 'a'+10));
+		ch2 = ((ch2 <= '9') ? (ch2 - '0') : (ch2 - 'a'+10));
+		m_visor[d] = 16*ch1 + ch2;
+		ch1 = m_god[d*2];
+		ch2 = m_god[d*2+1];
+		ch1 = ((ch1 <= '9') ? (ch1 - '0') : (ch1 - 'a'+10));
+		ch2 = ((ch2 <= '9') ? (ch2 - '0') : (ch2 - 'a'+10));
+		m_god[d] = 16*ch1 + ch2;
+	}
 }
 
 void Passwd::init()
 {
-   for( int i = 0; i < pswd_len; i++) characters[i] = '_';
-   for( int i = 0; i < key_len; i++)
-   {
-       visor[i] = '0';
-         god[i] = '0';
-      buffer[i] = '\0';
-   }
+	for (int i = 0; i < ms_pswd_len; i++)
+		m_characters[i] = '_';
+	for (int i = 0; i < ms_key_len; i++)
+	{
+		m_visor[i] = '0';
+		m_god[i] = '0';
+		m_buffer[i] = '\0';
+	}
 }
 
 Passwd::~Passwd()
 {
 }
 
-bool Passwd::checkKey( const char key, bool & VisorMode, bool & GodMode)
+bool Passwd::checkKey(const char i_key, bool & o_visor_mode, bool & o_god_mode)
 {
-   for( int i = 1; i < pswd_len; i++) characters[i-1] = characters[i];
-   characters[pswd_len-1] = key;
+	for (int i = 1; i < ms_pswd_len; i++)
+		m_characters[i-1] = m_characters[i];
+	m_characters[ms_pswd_len-1] = i_key;
 
+	calculate(m_digest, m_characters, ms_pswd_len);
 
-   calculate( digest, characters, pswd_len);
+	if (memcmp(m_visor, m_digest, ms_digest_len) == 0)
+	{
+		if (o_visor_mode)
+		{
+			o_visor_mode  = false;
+			printf("VISOR MODE OFF\n");
+			if (o_god_mode)
+				printf("GOD MODE OFF\n");
+			o_god_mode = false;
+		}
+		else
+		{
+			o_visor_mode  = true;
+			printf("VISOR MODE ON\n");
+			if (o_god_mode)
+				printf("GOD MODE OFF\n");
+			o_god_mode = false;
+		}
+		return true;
+	}
+	else if (memcmp(m_god, m_digest, ms_digest_len) == 0)
+	{
+		if (o_god_mode)
+		{
+			o_god_mode = false;
+			printf("GOD MODE OFF\n");
+			if (o_visor_mode)
+				printf("VISOR MODE OFF\n");
+			o_visor_mode = false;
+		}
+		else
+		{
+			o_god_mode  = true;
+			printf("GOD MODE ON\n");
+			if (!o_visor_mode)
+				printf("VISOR MODE ON\n");
+			o_visor_mode = true;
+		}
+		return true;
+	}
 
-//write(1,characters,pswd_len); write(1,"\n",1);
-//Passwd::PrintKey( digest);
-
-   if( memcmp( visor, digest, digest_len) == 0)
-   {
-      if( VisorMode)
-      {
-         VisorMode  = false;
-         printf("VISOR MODE OFF\n");
-         if( GodMode) printf("GOD MODE OFF\n");
-         GodMode = false;
-      }
-      else
-      {
-         VisorMode  = true;
-         printf("VISOR MODE ON\n");
-         if( GodMode) printf("GOD MODE OFF\n");
-         GodMode = false;
-      }
-      return true;
-   }
-   else if( memcmp( god, digest, digest_len) == 0)
-   {
-      if( GodMode)
-      {
-         GodMode = false;
-         printf("GOD MODE OFF\n");
-         if( VisorMode) printf("VISOR MODE OFF\n");
-         VisorMode  = false;
-      }
-      else
-      {
-         GodMode  = true;
-         printf("GOD MODE ON\n");
-         if( !VisorMode) printf("VISOR MODE ON\n");
-         VisorMode  = true;
-      }
-      return true;
-   }
-
-   return false;
+	return false;
 }
 
-void Passwd::calculate( unsigned char * key, const char * passwd)
+void Passwd::calculate(unsigned char * i_key, const char * i_passwd)
 {
-   int len = strlen( passwd);
-   calculate( key, (unsigned char *)passwd, len);
+	calculate(i_key, (unsigned char *)i_passwd, strlen(i_passwd));
 }
 
-void Passwd::PrintKey( unsigned char * key)
+void Passwd::PrintKey(unsigned char * i_key)
 {
-   for( int i = 0; i < Passwd::digest_len; i++) printf ("%02x", key[i]);
-   printf ("\n");
+	for (int i = 0; i < Passwd::ms_digest_len; i++)
+		printf("%02x", i_key[i]);
+	printf("\n");
 }
 
-void Passwd::calculate( unsigned char * key, unsigned char * passwd, int len)
+void Passwd::calculate(unsigned char * i_key, unsigned char * i_passwd, int i_len)
 {
-   MD5Init   ( &context);
-   MD5Update ( &context, passwd, len);
-   MD5Final  ( key, &context);
+	MD5Init  (&m_context);
+	MD5Update(&m_context, i_passwd, i_len);
+	MD5Final (i_key, &m_context);
 }
--- afanasy/src/libafanasy/common/passwd.h
+++ afanasy/src/libafanasy/common/passwd.h
@@ -10,34 +10,34 @@ namespace af
 class Passwd
 {
 public:
-   Passwd();
-   Passwd( const std::string & VisorKey, const std::string & GodKey);
-   ~Passwd();
+	Passwd();
+	Passwd(const std::string & i_visor_key, const std::string & i_god_key);
+	~Passwd();
 
-/// Check current key matching password sequence.
-   bool checkKey( const char key, bool & VisorMode, bool & GodMode);
+	/// Check current key matching password sequence.
+	bool checkKey(const char i_key, bool & o_visor_mode, bool & o_god_mode);
 
-   void calculate( unsigned char * key, const char * passwd);
+	void calculate(unsigned char * i_key, const char * i_passwd);
 
-   static void PrintKey( unsigned char * key);
+	static void PrintKey(unsigned char * i_key);
 
-   static const int digest_len = 16;
-   static const int key_len = 32;
-   static const int pswd_len = 5;
-
-private:
-   unsigned char visor[key_len];
-   unsigned char   god[key_len];
-   unsigned char characters[pswd_len];
-   unsigned char buffer[key_len];
+	static const int ms_digest_len = 16;
+	static const int ms_key_len = 32;
+	static const int ms_pswd_len = 5;
 
 private:
-   void init();
-   void calculate( unsigned char * key, unsigned char * passwd, int len);
+	unsigned char m_visor[ms_key_len];
+	unsigned char m_god[ms_key_len];
+	unsigned char m_characters[ms_pswd_len];
+	unsigned char m_buffer[ms_key_len];
 
 private:
-   MD5_CTX context;
-   unsigned char digest[digest_len];
+	void init();
+	void calculate(unsigned char * i_key, unsigned char * i_passwd, int i_len);
+
+private:
+	MD5_CTX m_context;
+	unsigned char m_digest[ms_digest_len];
 };
 }
 #endif
