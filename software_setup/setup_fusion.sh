180d35c701cad125a71126f6b45026af47e661cd
Erkan Ozgur Yilmaz <eoyilmaz@gmail.com>
2022-06-20 21:11:06 +0100

    * [Fusion] Updated ``setup_fusion.sh`` to setup the required environment variables for the ``FusionRenderNode`` executable, which is the correct executable to be used by farm managers. * [Fusion] Added the ``fusion_render_node`` shell script to ``software_setup/bin``.

--- software_setup/setup_fusion.sh
+++ software_setup/setup_fusion.sh
@@ -20,7 +20,7 @@ if [ -z "$FUSION_LOCATION" ]; then
   FUSION_EXEC=""
   for FUSION_FOLDER in $FUSION_FOLDERS ;
   do
-     if [ "`echo $FUSION_FOLDER | awk '{print match( \$1, "Fusion")}'`" == "1" ]; then
+     if [ "`echo $FUSION_FOLDER | awk '{print match( \$1, "Fusion[0-9]+")}'`" == "1" ]; then
         FUSION_LOCATION="${FUSION_INSTALL_DIR}/${FUSION_FOLDER}"
         if [ "`uname`" == "Darwin" ]; then
           FUSION_EXEC="${FUSION_FOLDER}.app/${FUSION_FOLDER}"
@@ -28,9 +28,18 @@ if [ -z "$FUSION_LOCATION" ]; then
           FUSION_EXEC="Fusion"
         fi
      fi
+     if [ "`echo $FUSION_FOLDER | awk '{print match( \$1, "FusionRenderNode[0-9]+")}'`" == "1" ]; then
+        FUSION_RENDERNODE_LOCATION="${FUSION_INSTALL_DIR}/${FUSION_FOLDER}"
+        if [ "`uname`" == "Darwin" ]; then
+          FUSION_RENDERNODE_EXEC="${FUSION_FOLDER}.app/${FUSION_FOLDER}"
+        else
+          FUSION_RENDERNODE_EXEC="FusionRenderNode"
+        fi
+     fi
   done
   export FUSION_LOCATION
   export FUSION_EXEC="${FUSION_LOCATION}/${FUSION_EXEC}"
+  export FUSION_RENDERNODE_EXEC="${FUSION_RENDERNODE_LOCATION}/${FUSION_RENDERNODE_EXEC}"
 else
   echo "FUSION_LOCATION is already set: ${FUSION_LOCATION}"
 fi
@@ -40,6 +49,8 @@ fi
 #
 export APP_DIR=$FUSION_LOCATION
 export APP_EXE=$FUSION_EXEC
+export RENDER_DIR=$FUSION_RENDERNODE_LOCATION
+export RENDER_EXE=$FUSION_RENDERNODE_EXEC
 
 #
 # Override fusion location based on locate_fusion.sh:
