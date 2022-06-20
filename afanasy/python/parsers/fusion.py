e569c2e6c55517061f0a05237da9d610b40fffed
Erkan Ozgur Yilmaz <eoyilmaz@gmail.com>
2022-06-20 21:47:32 +0100

    * [Fusion] Tidy up the ``fusion`` parser. But the code in its current form is not properly working and needs more attention.

--- afanasy/python/parsers/fusion.py
+++ afanasy/python/parsers/fusion.py
@@ -1,54 +1,38 @@
 # -*- coding: utf-8 -*-
 
+import os
+import re
+import tempfile
+
 from parsers import parser
 
-import tempfile
-import re
-
-# Render started at Thu 3:24PM  (Range: 0 to 10)
-# Rendered frame 0 (1 of 11), took 0.24 secs
-# Rendered frame 1 (2 of 11), took 0.51 secs
-# Rendered frame 2 (3 of 11), took 0.062 secs
-# Rendered frame 3 (4 of 11), took 0.103 secs
-# Rendered frame 4 (5 of 11), took 0.1 secs
-# Rendered frame 5 (6 of 11), took 0.069 secs
-# Rendered frame 6 (7 of 11), took 0.281 secs
-# Rendered frame 7 (8 of 11), took 0.047 secs
-# Rendered frame 8 (9 of 11), took 0.325 secs
-# Rendered frame 9 (10 of 11), took 0.07 secs
-# Rendered frame 10 (11 of 11), took 0.092 secs
-# Render completed successfully at Thu 3:24PM - Total Time: 0h 0m 1.66s, Average: 6.03 frames/second
-# Auto-exiting with errcode 0
-# Closing outstanding documents
-
-re_match_frame = re.compile(
-    r'(Rendered frame )(\d*)( \()(\d*)( of )(\d*)'
-)
-re_match_complete = re.compile('Render completed.*')
+re_match_frame = re.compile(r"(Rendered frame )(\d*)( \()(\d*)( of )(\d*)")
+re_match_complete = re.compile("Render completed.*")
 
 
 class fusion(parser.parser):
-    """fusion parser
-    """
+    """fusion parser"""
 
     def __init__(self):
         parser.parser.__init__(self)
 
-        self.str_error = ['[mtoa] Failed batch render']
-        self.log_file = \
-            ('%s/fusion_render.log' % tempfile.gettempdir()).replace('\\', '/')
-        self.str_finishedsuccess = ['Closing outstanding documents']
+        self.str_error = ["Error "]
+        self.log_file = ("%s/fusion_render.log" % tempfile.gettempdir()).replace(
+            "\\", "/"
+        )
+        self.str_finishedsuccess = ["Closing outstanding documents"]
         self.last_read_line = 0
 
         # delete the log file beforehand
         import os
+
         try:
             os.remove(self.log_file)
         except (OSError, IOError):
             pass
 
     def do(self, i_args):
-        data = i_args['data']
+        data = i_args["data"]
 
         # the data is always empty
         # use the %TEMP%/fusion_render.log
@@ -65,7 +49,7 @@ class fusion(parser.parser):
             return
 
         # read new lines
-        self.log = '\n'.join(data)
+        self.log = "\n".join(data)
         for i in range(self.last_read_line, current_line_count):
             self.last_read_line = i
             line = data[i].strip()
@@ -82,7 +66,6 @@ class fusion(parser.parser):
                 self.finishedsuccess = True
                 # delete the log file
 
-                import os
                 try:
                     os.remove(self.log_file)
                 except (OSError, IOError):
