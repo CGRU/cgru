47a7058e7ad62edb017580a13bbde48718436b09
Luke Whitehorn <luke.whitehorn@gmail.com>
2022-11-28 16:28:05 +0100

    Do not override AE_LOCATION if it is already defined before calling setup_afterfx.cmd

--- software_setup/setup_afterfx.cmd
+++ software_setup/setup_afterfx.cmd
@@ -1,13 +1,11 @@
 rem Source general for all soft directives:
 call %CGRU_LOCATION%\software_setup\setup__all.cmd
 
-
-set "ADOBE_LOCATION=C:\Program Files\Adobe"
-
-set "AE_SEARCH=%ADOBE_LOCATION%\Adobe After Effects"
-For /F "Tokens=*" %%I in ('dir /b "%AE_SEARCH%*"') Do set "AE_LOCATION=%%I"
-
-set "AE_LOCATION=%ADOBE_LOCATION%\%AE_LOCATION%\Support Files"
+IF NOT DEFINED AE_LOCATION (
+    For /F "Tokens=*" %%I in ('dir /b "C:\Program Files\Adobe\Adobe After Effects*"') Do set "AE_LOCATION=C:\Program Files\Adobe\%%I\Support Files"
+) ELSE (
+    echo "AE_LOCATION already set: %AE_LOCATION%"
+)
 
 set APP_DIR=%AE_LOCATION%
 set APP_EXE=%AE_LOCATION%\aerender.exe
