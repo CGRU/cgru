2e75d3e47c6a8292d5d757aaa8bca9ca25c0031a
timurhai <timurhai@gmail.com>
2023-01-23 13:42:02 +0300

    houdini cmd scripts updated to use hython.exe and not hython{python version}.exe

--- software_setup/bin/htracker.cmd
+++ software_setup/bin/htracker.cmd
@@ -2,7 +2,7 @@
 
 call "%CGRU_LOCATION%\software_setup\setup_houdini.cmd"
 
-set "PYTHONPATH=%HOUDINI_LOCATION%\houdini\python2.7libs;%PYTHONPATH%"
+set "PYTHONPATH=%HOUDINI_LOCATION%\houdini\python3.9libs;%PYTHONPATH%"
 
-"%HOUDINI_LOCATION%\bin\hython2.7" "%HOUDINI_CGRU_PATH%\htracker.py" %*
+"%HOUDINI_LOCATION%\bin\hython" "%HOUDINI_CGRU_PATH%\htracker.py" %*
 
