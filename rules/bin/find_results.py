96ebd2c6bc007a4a464ae5a63ff1e044776ffd1c
timurhai <timurhai@gmail.com>
2023-02-23 17:25:40 +0300

    rules: find results: movie files support

--- rules/bin/find_results.py
+++ rules/bin/find_results.py
@@ -10,9 +10,9 @@ import sys
 from optparse import OptionParser
 
 Parser = OptionParser(
-	usage="%prog [options] paths\n"
-		  "   Type \"%prog -h\" for help",
-	version="%prog 1.0"
+    usage="%prog [options] paths\n"
+          "   Type \"%prog -h\" for help",
+    version="%prog 1.0"
 )
 
 Parser.add_option('-r', '--respaths',     dest='respaths',     type  ='string',     default='RESULT/JPG,RESULT/TIF,RESULT/DPX')
@@ -30,104 +30,113 @@ Results = []
 Out['results'] = Results
 
 def Output():
-	json.dump({"find_results":Out}, sys.stdout, indent=1)
-	sys.stdout.write('\n')
+    json.dump({"find_results":Out}, sys.stdout, indent=1)
+    sys.stdout.write('\n')
 
 def errExit(i_msg):
-	Out['error'] = i_msg
-	Out['status'] = 'error'
-	Output()
-	sys.exit(1)
+    Out['error'] = i_msg
+    Out['status'] = 'error'
+    Output()
+    sys.exit(1)
 
 def interrupt(signum, frame):
-	errExit('Interrupt received')
+    errExit('Interrupt received')
 
 signal.signal(signal.SIGTERM, interrupt)
 signal.signal(signal.SIGABRT, interrupt)
 signal.signal(signal.SIGINT, interrupt)
 
 if len(args) < 1:
-	errExit('Not enough arguments provided.')
+    errExit('Not enough arguments provided.')
 
 DestFiles = None
 if Options.dest is not None:
-	Out['dest'] = Options.dest
-	if not Options.skipcheck:
-		if not os.path.isdir( Options.dest):
-			errExit('Destination folder does not exist: ' + Options.dest)
-		DestFiles = os.listdir( Options.dest)
+    Out['dest'] = Options.dest
+    if not Options.skipcheck:
+        if not os.path.isdir( Options.dest):
+            errExit('Destination folder does not exist: ' + Options.dest)
+        DestFiles = os.listdir( Options.dest)
 
 ResPaths = Options.respaths.split(',')
 
 SimilarCharactrers = ' .-()[]{}!'
 def simiralName(i_name):
-	name = i_name.lower()
-	for c in SimilarCharactrers:
-		name = name.replace(c, '_')
-	return name
+    name = i_name.lower()
+    for c in SimilarCharactrers:
+        name = name.replace(c, '_')
+    return name
 
 for src in args:
 
-	result = dict()
-	result['src'] = None
+    result = dict()
+    result['src'] = None
 
-	version = None
-	name = os.path.basename(src)
+    version = None
+    name = os.path.basename(src)
 
-	for res in ResPaths:
-		respath = os.path.join(src, res)
-		if not os.path.isdir(respath):
-			continue
+    for res in ResPaths:
+        respath = os.path.join(src, res)
+        if not os.path.isdir(respath):
+            continue
 
-		for item in os.listdir(respath):
-			if item[0] in '._':
-				continue
+        for item in os.listdir(respath):
+            if item[0] in '._':
+                continue
 
-			if not os.path.isdir(os.path.join(respath, item)):
-				continue
+            path = os.path.join(respath, item)
+            #if not os.path.isdir(path):
+            #    continue
 
-			ver = simiralName(item).replace(simiralName(name), '').strip('!_-. ')
+            ver = simiralName(item).replace(simiralName(name), '').strip('!_-. ')
 
-			ver_digits = re.findall(r'\d+', ver)
-			if len(ver_digits):
-				ver_digits = ver_digits[0]
-				ver_number = int(ver_digits)
-				ver_number = ('%0' + str(Options.padding) + 'd') % ver_number
-				ver = ver.replace(ver_digits, ver_number, 1)
+            ver_digits = re.findall(r'\d+', ver)
+            if len(ver_digits):
+                ver_digits = ver_digits[0]
+                ver_number = int(ver_digits)
+                ver_number = ('%0' + str(Options.padding) + 'd') % ver_number
+                ver = ver.replace(ver_digits, ver_number, 1)
 
-			if len(ver) and ver[0].isdigit():
-				ver = 'v' + ver
+            if len(ver) and ver[0].isdigit():
+                ver = 'v' + ver
 
-			if version is not None:
-				if version >= ver:
-					continue
-			version = ver
+            if version is not None:
+                if version >= ver:
+                    continue
 
-			result['src'] = os.path.join(respath, item)
-			result['respath'] = res
+            version = ver
+            version = version.split('_')[0]
+            if os.path.isfile(path):
+                version, ext = os.path.splitext(version)
+                result['file'] = item
 
-	if result['src'] is None:
-		result['error'] = 'Not found'
-		if not Options.skiperrors:
-			errExit('Input not found for: %s' % src)
+            result['src'] = os.path.join(respath, item)
+            result['respath'] = res
 
-	if version == '' or version is None:
-		version = ('v%0' + str(Options.padding) + 'd') % 0
-	name += '_' + version
+    if result['src'] is None:
+        result['error'] = 'Not found'
+        if not Options.skiperrors:
+            errExit('Input not found for: %s' % src)
 
-	result['name'] = name
-	result['version'] = version
-	result['asset'] = src
+    if version == '' or version is None:
+        version = ('v%0' + str(Options.padding) + 'd') % 0
+    name += '_' + version
 
-	if Options.dest is not None:
-		result['dest'] = os.path.join(Options.dest, name)
-		if DestFiles is not None:
-			for afile in DestFiles:
-				if afile.find( result['name']) == 0:
-					result['exist'] = True
-					break
+    result['name'] = name
+    result['version'] = version
+    result['asset'] = src
 
-	Results.append(result)
+    if Options.dest is not None:
+        if 'file' in result:
+            result['dest'] = os.path.join(Options.dest, result['file'])
+        else:
+            result['dest'] = os.path.join(Options.dest, name)
+        if DestFiles is not None:
+            for afile in DestFiles:
+                if afile.find( result['name']) == 0:
+                    result['exist'] = True
+                    break
+
+    Results.append(result)
 
 
 Out['info'] = "%d sequences found" % len(Results)
