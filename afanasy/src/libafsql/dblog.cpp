70b06b1126d0ab110ae6504262af9d9745b7f8db
timurhai <timurhai@gmail.com>
2025-04-14 21:05:27 +0300

    afanasy: DBLog class added (ocasionally missed in a previous commit)

---
+++ afanasy/src/libafsql/dblog.cpp
@@ -0,0 +1,33 @@
+#include "dblog.h"
+
+#include "../libafanasy/log.h"
+
+#include "dbattr.h"
+
+using namespace afsql;
+
+const std::string DBLog::ms_TableName("logs");
+
+DBLog::DBLog()
+{
+	dbAddAttr(new DBAttrInt64 (DBAttr::_time,    &ltime  ));
+	dbAddAttr(new DBAttrString(DBAttr::_subject, &subject));
+	dbAddAttr(new DBAttrString(DBAttr::_type,    &type   ));
+	dbAddAttr(new DBAttrString(DBAttr::_object,  &object ));
+	dbAddAttr(new DBAttrString(DBAttr::_info,    &info   ));
+}
+
+DBLog::~DBLog()
+{
+}
+
+void DBLog::add(const af::Log * i_log, std::list<std::string> * o_queries)
+{
+	ltime   = i_log->ltime;
+	subject = i_log->subject;
+	type    = i_log->type;
+	object  = i_log->object;
+	info    = i_log->info;
+
+	v_dbInsert( o_queries);
+}
---
+++ afanasy/src/libafsql/dblog.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "../libafanasy/name_af.h"
+
+#include "dbitem.h"
+
+namespace afsql
+{
+class DBLog: public DBItem
+{
+public:
+	DBLog();
+	virtual ~DBLog();
+
+	void add(const af::Log * i_log, std::list<std::string> * o_queries);
+
+	inline const std::string & v_dbGetTableName() const {return ms_TableName;}
+
+private:
+	int64_t ltime;
+	std::string subject;
+	std::string type;
+	std::string object;
+	std::string info;
+
+private:
+	static const std::string ms_TableName;
+};
+}
