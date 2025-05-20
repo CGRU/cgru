402d0f71913cd900225dc10b26af1c5e0567dc59
timurhai <timurhai@gmail.com>
2025-05-20 11:18:08 +0300

    afstatistics: server test script

--- afanasy/statistics/test.py
+++ afanasy/statistics/test.py
@@ -12,50 +12,41 @@ import afstatsrv
 
 r = afstatsrv.Requests()
 
-out = dict()
-r.req_init(None, out)
-print(json.dumps(out, indent=4))
+def request(i_name, i_args = None):
+    func = 'req_' + i_name
+    print('\n' + '#'*8 + ' ' + func)
+    print(json.dumps(i_args, indent=4))
+    out = dict()
+    getattr(r, func)(i_args, out)
+    print('# out:')
+    print(json.dumps(out, indent=4))
+    return out
+
+out = request('init')
 folder = out['folders'][0]
-print('Folder = "%s"' % folder)
+print('# Folder = "%s"' % folder)
 
 args = dict()
-args['table'] = 'jobs'
 args['select'] = 'folder'
 #args['time_max'] = int(time.time())
 args['time_min'] = int(time.time()) - 60*60 * 24 * 48
 args['folder'] = folder
-out = dict()
-r.req_get_jobs_folders(args, out)
-print(json.dumps(out, indent=4))
+out = request('get_jobs_folders', args)
 
 args['favorite'] = 'username'
-out = dict()
-r.req_get_jobs_table(args, out)
-print(json.dumps(out, indent=4))
+out = request('get_jobs_table', args)
 
-out = dict()
-r.req_get_tasks_folders(args, out)
-print(json.dumps(out, indent=4))
+out = request('get_tasks_folders', args)
 
-out = dict()
-r.req_get_tasks_table(args, out)
-print(json.dumps(out, indent=4))
+out = request('get_tasks_table', args)
 
 args['interval'] = 1000000
-out = dict()
-r.req_get_tasks_folders_graph(args, out)
-print(json.dumps(out, indent=4))
+out = request('get_tasks_folders_graph', args)
 
-out = dict()
-r.req_get_tasks_graph(args, out)
-print(json.dumps(out, indent=4))
+out = request('get_tasks_graph', args)
 
-out = dict()
 args['folder'] = '/xxx'
-r.req_folder_delete(args, out)
-print(json.dumps(out, indent=4))
+out = request('folder_delete', args)
 
-out = dict()
 args['order'] = 'time'
-r.req_get_logs_table(args, out)
-print(json.dumps(out, indent=4))
+out = request('get_logs_table', args)
